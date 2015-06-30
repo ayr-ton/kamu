e9bba465b5535dcde31abb1651bb20a86a84ac67
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2015-06-30 13:57:12 -0300

    Generate Yeoman Angular basic structure

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name libraryUiApp
+ * @description
+ * # libraryUiApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('libraryUiApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
