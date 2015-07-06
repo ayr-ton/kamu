d3639d5f9b8e41f4a9e84a0680e8acb785b2caa9
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2015-07-06 14:33:51 -0300

    Corrigindo sintaxe

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,14 +13,14 @@ angular
 	.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
 
 
-  		var endPoint = "https://tw-library-api.herokuapp.com/books";
+  		var endPoint = 'https://tw-library-api.herokuapp.com/books';
 
 		function findBooks() {
 		        
 		        //get all tasks and display initially
 		        $http.get(endPoint).
 		            success(function (data) {
-		                if (data._embedded != undefined) {
+		                if (data._embedded !== undefined) {
 		                	angular.forEach(data._embedded, function (item) {
 		                        $scope.books = item;
 		                    });
