db662b75d5740b9bad0e31f41bc45847d7405890
Boadu Akoto Charles Jnr <ekowcharles@yahoo.co.uk>
2015-07-21 14:07:00 -0300

    @ekowcharles: running diagnostics

--- Gruntfile.js
+++ Gruntfile.js
@@ -71,10 +71,10 @@ module.exports = function (grunt) {
           livereload: '<%= connect.options.livereload %>'
         }
       },
-      jsTest: {
-        files: ['test/spec/**/*.js'],
-        tasks: ['newer:jshint:test', 'karma']
-      },
+      // jsTest: {
+      //   files: ['test/spec/**/*.js'],
+      //   tasks: ['newer:jshint:test', 'karma']
+      // },
       styles: {
         files: ['<%= yeoman.app %>/styles/**/*.css'],
         tasks: ['newer:copy:styles', 'autoprefixer']
