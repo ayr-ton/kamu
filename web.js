0f22026800a1d9943cb4d42e470204559a4c6937
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2015-08-07 20:13:00 -0300

    replaces gzippo with compression for ui asset compression

--- web.js
+++ web.js
@@ -1,8 +1,9 @@
-var gzippo = require('gzippo');
+var compression = require('compression')();
 var express = require('express');
 var morgan = require('morgan');
 var app = express();
 
 app.use(morgan('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.use(compression);
+app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 5000);
