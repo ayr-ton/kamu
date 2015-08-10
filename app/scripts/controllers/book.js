63ddd059f29eec903a98de81c45a4fa372de5749
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2015-08-10 19:05:37 -0300

    [#34] Ensures that returned book's ISBN_13 matches exactly the one provided by user.

--- app/scripts/controllers/book.js
+++ app/scripts/controllers/book.js
@@ -7,35 +7,39 @@ angular
     $scope.searchCriteria = "";
 
     $scope.findGoogleBooks = function findGoogleBooks() {
-        if($scope.searchCriteria.toString() != "") {
-            var endPoint = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + $scope.searchCriteria.toString();
+        var searchCriteria = $scope.searchCriteria.toString();
+
+        if(searchCriteria != "") {
+            var endPoint = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + searchCriteria;
+
+            console.log(endPoint);
 
             $http.get(endPoint).
                 success(function (data) {
-                	$scope.books = [];
                     $scope.book = {};
 
                     if (data.total_items !== 0) {
                  		angular.forEach(data.items, function (item) {
-                 			var book = {};
                             var bookInfo = item.volumeInfo;
 
-                 			book.title = bookInfo.title;
-                 			book.author = bookInfo.authors.join(",");
-                 			book.subtitle = bookInfo.subtitle;
-                 			book.description = bookInfo.description;
                  			angular.forEach(bookInfo.industryIdentifiers, function (isbn) {
-                 				if (isbn.type === 'ISBN_13') {
+                 				if (isbn.type === 'ISBN_13' && isbn.identifier == searchCriteria) {
+                                    var book = {};
+
+                                    book.title = bookInfo.title;
+                                    book.author = bookInfo.authors.join(",");
+                                    book.subtitle = bookInfo.subtitle;
+                                    book.description = bookInfo.description;
                  					book.isbn13 = isbn.identifier;
+                                    book.publisher = bookInfo.publisher;
+                                    book.publicationDate = bookInfo.publishedDate;
+                                    book.numberOfPages = bookInfo.pageCount;
+                                    book.imageUrl = bookInfo.imageLinks.thumbnail;
+                                     
+                                    $scope.book = book;
                  				}
                  			});
-                 			book.publisher = bookInfo.publisher;
-                 			book.publicationDate = bookInfo.publishedDate;
-                 			book.numberOfPages = bookInfo.pageCount;
-                 			book.imageUrl = bookInfo.imageLinks.thumbnail;
-                 			 
-                            $scope.book = book;
-                            $scope.books = book;
+                 			
                         });
                     }
                });
