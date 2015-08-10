8073ed77714d49c771ff6cdae3ab3f81e32804c7
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2015-08-10 18:34:54 -0300

    [#34] Auto-completes form information for a single book

--- app/scripts/controllers/book.js
+++ app/scripts/controllers/book.js
@@ -3,34 +3,43 @@
 angular
   .module('libraryUiApp')
   .controller('BookCtrl', function($scope, $http) {
-	var endPoint = "https://www.googleapis.com/books/v1/volumes?q=isbn:9780932633019";
 
-   $scope.findGoogleBooks = function findGoogleBooks() {
-        $http.get(endPoint).
-            success(function (data) {
-            	$scope.books = [];
-                $scope.book = {};
+    $scope.searchCriteria = "";
 
-                if (data.total_items !== 0) {
-             		angular.forEach(data.items, function (item) {
-             			var book = {};
-             			book.title = item.volumeInfo.title;
-             			book.author = item.volumeInfo.authors.join(",");
-             			book.subtitle = item.volumeInfo.subtitle;
-             			book.description = item.volumeInfo.description;
-             			angular.forEach(item.volumeInfo.industryIdentifiers, function (isbn) {
-             				if (isbn.type === 'ISBN_13') {
-             					book.isbn13 = isbn.identifier;
-             				}
-             			});
-             			book.publisher = item.volumeInfo.publisher;
-             			book.publicationDate = item.volumeInfo.publishedDate;
-             			book.numberOfPages = item.volumeInfo.pageCount;
-             			book.imageUrl = item.volumeInfo.imageLinks[0];
-             			 
-                        $scope.books = book;
-                    });
-                }
-           });
-    	}
-  });
+    $scope.findGoogleBooks = function findGoogleBooks() {
+        if($scope.searchCriteria.toString() != "") {
+            var endPoint = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + $scope.searchCriteria.toString();
+
+            $http.get(endPoint).
+                success(function (data) {
+                	$scope.books = [];
+                    $scope.book = {};
+
+                    if (data.total_items !== 0) {
+                 		angular.forEach(data.items, function (item) {
+                 			var book = {};
+                            var bookInfo = item.volumeInfo;
+
+                 			book.title = bookInfo.title;
+                 			book.author = bookInfo.authors.join(",");
+                 			book.subtitle = bookInfo.subtitle;
+                 			book.description = bookInfo.description;
+                 			angular.forEach(bookInfo.industryIdentifiers, function (isbn) {
+                 				if (isbn.type === 'ISBN_13') {
+                 					book.isbn13 = isbn.identifier;
+                 				}
+                 			});
+                 			book.publisher = bookInfo.publisher;
+                 			book.publicationDate = bookInfo.publishedDate;
+                 			book.numberOfPages = bookInfo.pageCount;
+                 			book.imageUrl = bookInfo.imageLinks.thumbnail;
+                 			 
+                            $scope.book = book;
+                            $scope.books = book;
+                        });
+                    }
+               });
+        }
+    }
+
+});
