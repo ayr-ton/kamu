2ec2edb7bc573ef63309c457cb4e254694d84afd
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2015-08-11 11:37:21 -0300

    [#34] refactors book extraction process

--- app/scripts/controllers/book.js
+++ app/scripts/controllers/book.js
@@ -6,47 +6,55 @@ angular
 
     $scope.searchCriteria = "";
 
-
     $scope.findGoogleBooks = function findGoogleBooks() {
         var searchCriteria = $scope.searchCriteria.toString();
+        $scope.book = {};
 
-        if(searchCriteria != "") {
+        if (searchCriteria != "") {
             var endPoint = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + searchCriteria;
 
             $http.get(endPoint).
                 success(function (data) {
-
                     angular.forEach(data.items, function (item) {
-                        var bookInfo = item.volumeInfo;
-                        
-                        $scope.book = {};
-                        
-                        angular.forEach(bookInfo.industryIdentifiers, function (isbn) {
-                            if (isbn.type === 'ISBN_13' && isbn.identifier == searchCriteria) {
-                                var book = {};
-
-                                book.title = bookInfo.title;
-                                book.author = bookInfo.authors;
-                                book.subtitle = bookInfo.subtitle;
-                                book.description = bookInfo.description;
-                                book.isbn13 = isbn.identifier;
-                                book.publisher = bookInfo.publisher;
-                                book.publicationDate = bookInfo.publishedDate;
-                                book.numberOfPages = bookInfo.pageCount;
-
-                                if (bookInfo.imageLinks !== undefined) {
-                                    book.imageUrl = bookInfo.imageLinks.thumbnail;
-                                } else {
-                                    book.imageUrl = "images\\no-image.png";
-                                }
-
-                                $scope.book = book;
-                            }
-                        });
-                        
+                        $scope.book = extractBookInformation(item.volumeInfo, searchCriteria)
                     });
                });
         }
+    }
+
+    function extractBookInformation(bookInfo, searchCriteria) {
+        var targetBookIdentifier = 'ISBN_13';
+
+        var book = {}
+
+        angular.forEach(bookInfo.industryIdentifiers, function (isbn) {
+            if (isbn.type === targetBookIdentifier && isbn.identifier === searchCriteria) {
+                book = buildBook(bookInfo, isbn);
+            }
+        });
+
+        return book;
+    }
+
+    function buildBook(bookInfo, isbn) {
+        var book = {};
+
+        book.title = bookInfo.title;
+        book.author = bookInfo.authors;
+        book.subtitle = bookInfo.subtitle;
+        book.description = bookInfo.description;
+        book.isbn13 = isbn.identifier;
+        book.publisher = bookInfo.publisher;
+        book.publicationDate = bookInfo.publishedDate;
+        book.numberOfPages = bookInfo.pageCount;
+
+        if (bookInfo.imageLinks !== undefined) {
+            book.imageUrl = bookInfo.imageLinks.thumbnail;
+        } else {
+            book.imageUrl = "images\\no-image.png";
+        }
+
+        return book;
     }
 
 });
