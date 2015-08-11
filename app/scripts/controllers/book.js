d55891883727bce0b747c36604ba33d2bf155266
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2015-08-11 11:13:07 -0300

    [#34] shows placeholder image when book image is not available in google api

--- app/scripts/controllers/book.js
+++ app/scripts/controllers/book.js
@@ -6,42 +6,45 @@ angular
 
     $scope.searchCriteria = "";
 
+
     $scope.findGoogleBooks = function findGoogleBooks() {
         var searchCriteria = $scope.searchCriteria.toString();
 
         if(searchCriteria != "") {
             var endPoint = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + searchCriteria;
 
-            console.log(endPoint);
-
             $http.get(endPoint).
                 success(function (data) {
-                    $scope.book = {};
 
-                    if (data.total_items !== 0) {
-                 		angular.forEach(data.items, function (item) {
-                            var bookInfo = item.volumeInfo;
+                    angular.forEach(data.items, function (item) {
+                        var bookInfo = item.volumeInfo;
+                        
+                        $scope.book = {};
+                        
+                        angular.forEach(bookInfo.industryIdentifiers, function (isbn) {
+                            if (isbn.type === 'ISBN_13' && isbn.identifier == searchCriteria) {
+                                var book = {};
 
-                 			angular.forEach(bookInfo.industryIdentifiers, function (isbn) {
-                 				if (isbn.type === 'ISBN_13' && isbn.identifier == searchCriteria) {
-                                    var book = {};
+                                book.title = bookInfo.title;
+                                book.author = bookInfo.authors;
+                                book.subtitle = bookInfo.subtitle;
+                                book.description = bookInfo.description;
+                                book.isbn13 = isbn.identifier;
+                                book.publisher = bookInfo.publisher;
+                                book.publicationDate = bookInfo.publishedDate;
+                                book.numberOfPages = bookInfo.pageCount;
 
-                                    book.title = bookInfo.title;
-                                    book.author = bookInfo.authors.join(",");
-                                    book.subtitle = bookInfo.subtitle;
-                                    book.description = bookInfo.description;
-                 					book.isbn13 = isbn.identifier;
-                                    book.publisher = bookInfo.publisher;
-                                    book.publicationDate = bookInfo.publishedDate;
-                                    book.numberOfPages = bookInfo.pageCount;
+                                if (bookInfo.imageLinks !== undefined) {
                                     book.imageUrl = bookInfo.imageLinks.thumbnail;
-                                     
-                                    $scope.book = book;
-                 				}
-                 			});
-                 			
+                                } else {
+                                    book.imageUrl = "images\\no-image.png";
+                                }
+
+                                $scope.book = book;
+                            }
                         });
-                    }
+                        
+                    });
                });
         }
     }
