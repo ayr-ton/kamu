9e4f66ddf6cfc734bd31f92d384b184301f91228
Raquel Lemos <raqlem@gmail.com>
2015-08-12 16:02:13 -0300

    [#34] Applying UX for auto-complete book information
    @ekowcharles / @raqlem

--- app/scripts/controllers/book.js
+++ app/scripts/controllers/book.js
@@ -20,16 +20,69 @@ angular
                         $scope.book = data._embedded.books[0];
 
                         $scope.book.imageUrl = BookService.resolveBookImage($scope.book.imageUrl);
+
+                        angular.element('#search-result').removeClass('hide');
+                        angular.element('#donator').removeClass('hide');
+                        angular.element('#add-book').removeClass('hide');
+                        angular.element('#not-found-message').addClass('hide');
                     } else {
                         BookService.findGoogleBooks(searchCriteria).
                             success(function (data) {
+                                var bookFound = false;
+
                                 angular.forEach(data.items, function (item) {
                                     $scope.book = BookService.extractBookInformation(item.volumeInfo, searchCriteria);
+
+                                    angular.element('#search-result').removeClass('hide');
+                                    angular.element('#not-found-message').addClass('hide');
+                                    angular.element('#donator').removeClass('hide');
+                                    angular.element('#add-book').removeClass('hide');
+
+                                    bookFound = true;
                                 });
+
+                                if(!bookFound) {
+                                    angular.element('#not-found-message').removeClass('hide');
+                                    angular.element('#search-result').addClass('hide');
+                                    angular.element('#donator').addClass('hide');
+                                    angular.element('#add-book').addClass('hide');
+
+                                }
+                            }).
+                            error(function() {
+                                angular.element('#not-found-message').removeClass('hide');
+                                angular.element('#search-result').addClass('hide');
+                                angular.element('#donator').addClass('hide');
+                                angular.element('#add-book').addClass('hide');
+
                             });
                     }
 
                 });
         }   
     };
+
+    $scope.autoCompleteSearch = function () {
+        angular.element('#search').removeClass('hide');
+        angular.element('#not-found-message').addClass('hide');
+        angular.element('#search-result').addClass('hide');
+        angular.element('#donator').addClass('hide');
+        angular.element('#add-book').addClass('hide');
+        angular.element('#isbn-search').removeClass('popup-nav-unactive');
+        angular.element('#isbn-search').addClass('popup-nav-active');
+        angular.element('#manual-add').removeClass('popup-nav-active');
+        angular.element('#manual-add').addClass('popup-nav-unactive');
+    }
+
+    $scope.addManually = function() {
+        angular.element('#search').addClass('hide');
+        angular.element('#not-found-message').addClass('hide');
+        angular.element('#search-result').removeClass('hide');
+        angular.element('#donator').removeClass('hide');
+        angular.element('#add-book').removeClass('hide');
+        angular.element('#isbn-search').removeClass('popup-nav-active');
+        angular.element('#isbn-search').addClass('popup-nav-unactive');
+        angular.element('#manual-add').removeClass('popup-nav-unactive');
+        angular.element('#manual-add').addClass('popup-nav-active');
+    }
 });
