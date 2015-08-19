479bbb1a24f2b7c11d68ae84035aa72280743dad
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2015-08-19 14:58:12 -0300

    [#41] Prefixes urls with library

--- test/spec/routes/routes_spec.js
+++ test/spec/routes/routes_spec.js
@@ -2,37 +2,56 @@
 
 describe('libraryUIApp routing', function() {
   
-  var location, route, rootScope;
+  var location, route, rootScope, routeParams;
 
   beforeEach(module('libraryUiApp'));
+  beforeEach(module('ngRoute'));
 
 
   beforeEach(inject(
-    function( _$location_, _$route_, _$rootScope_ ) {
+    function(_$location_, _$route_, _$rootScope_, _$routeParams_) {
       location = _$location_;
       route = _$route_;
       rootScope = _$rootScope_;
+      routeParams = _$routeParams_;
     }));
 
    describe('case insensitive routes', function() {
       beforeEach(inject(
         function($httpBackend) {
-            $httpBackend.expectGET('views/book/add-book.html')
-            .respond(200);
+          $httpBackend.expectGET('views/book/add-book.html')
+          .respond(200);
         }));
 
       it('should route correctly when route is lower case', function() {
-          location.path('/add_book');
-          rootScope.$digest();
+        location.path('/library/quito/add_book');
+        rootScope.$digest();
 
-          expect(route.current.controller).toBe('BookCtrl');
+        expect(route.current.controller).toBe('BookCtrl');
       });
 
       it('should route correctly when route is upper case', function() {
-          location.path('/ADD_BOOK');
-          rootScope.$digest();
+        location.path('/library/quito/ADD_BOOK');
+        rootScope.$digest();
 
-          expect(route.current.controller).toBe('BookCtrl');
+        expect(route.current.controller).toBe('BookCtrl');
       });
-  });    
+  });
+
+  describe('library dymanic routes', function() {
+    beforeEach(inject(
+        function($httpBackend) {
+          $httpBackend.expectGET('views/book/add-book.html')
+          .respond(200);
+        }));
+
+    it('correctly identifies library param', function() {
+      inject(function($route) {
+        location.path('/library/random/add_book');
+        rootScope.$digest();
+
+        expect(route.current.pathParams.library).toBe('random');
+      });
+    });
+  });
 });
