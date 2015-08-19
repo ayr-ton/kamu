9268f2ecc65bc3fc5f89764de6af0a44d384932e
Boadu Akoto Charles Jnr <ekowcharles@yahoo.co.uk>
2015-08-19 11:27:28 -0300

    [#41] Removes about page from application

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -35,10 +35,6 @@ angular
         templateUrl: 'views/book/add-book.html',
         controller: 'BookCtrl'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
       .otherwise({
         redirectTo: '/'
       });
