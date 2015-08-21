822deae006f8ac9726227e7d0ed2796f49d2da2d
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2015-08-21 13:12:46 -0300

    [#41] Extracts library api call into a service

---
+++ test/spec/services/library_spec.js
@@ -0,0 +1,19 @@
+'use strict';
+
+describe('LibraryService', function(){
+
+  beforeEach(module('libraryUiApp'));
+
+  describe('listLibraries', function(){
+    it ('requests list of libraries from api', inject(function($injector, $http, ENV){
+      var libraryService = $injector.get('LibraryService')
+      var url = ENV.apiEndpoint + '/libraries';
+
+      spyOn($http, 'get');
+
+      libraryService.getLibraries();
+
+      expect($http.get).toHaveBeenCalledWith(url);
+    }));
+  });
+});
