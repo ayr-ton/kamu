5aaef3cb7305b79b51f4624604ce118892ceddd7
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2015-08-26 14:39:30 -0300

    [#47] Tests modals.params

--- test/spec/services/modal_spec.js
+++ test/spec/services/modal_spec.js
@@ -4,15 +4,28 @@ describe('Modals', function () {
 
   beforeEach(module('libraryUiApp'));
 
-  describe('Open', function () {
+  describe('#open', function () {
     it('emits modal.open message', inject(function ($rootScope, $q, modals) {
       spyOn($rootScope, '$emit');
 
       var promise = modals.open(window, {}, false);
-      promise = modals.open(window, {}, false);
 
       expect($rootScope.$emit).toHaveBeenCalledWith('modals.open', window);
       expect(promise).toEqual($q.defer().promise);
+    }));
+  });
+
+  describe('#params', function() {
+    it('returns params when set', inject(function ($rootScope, $q, modals) {
+      var params = { "param": "someValue" };
+
+      var promise = modals.open(window, params, false);
+
+      expect(modals.params()).toEqual(params);
+    }));
+
+    it('returns empty hash when not set', inject(function ($rootScope, $q, modals) {
+      expect(modals.params()).toEqual({});
     }));
   });
 });
