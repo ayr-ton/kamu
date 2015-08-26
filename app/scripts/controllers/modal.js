af4259d723277bf2b2c779f14e88d0afb1680c15
Boadu Akoto Charles Jnr <ekowcharles@yahoo.co.uk>
2015-08-26 18:10:45 -0300

    [#47] Tests modal controllers
    @ffrancieli / @ekowcharles

--- app/scripts/controllers/modal.js
+++ app/scripts/controllers/modal.js
@@ -2,9 +2,15 @@
 
 var app = angular.module('libraryUiApp');
 
+app.controller('ModalCtrl', function ($scope, Modal) {
+    $scope.cancel = function() {
+      Modal.reject();
+    };
+  }
+);
+
 app.controller('BorrowModalCtrl', function ($scope, Modal) {
     $scope.copy = Modal.params().copy;
-    $scope.cancel = Modal.reject;
 
     $scope.submit = function () {
       var loan = { copy: $scope.copy, email: $scope.form.user + '@thoughtworks.com' };
@@ -15,7 +21,6 @@ app.controller('BorrowModalCtrl', function ($scope, Modal) {
 
 app.controller('ReturnModalCtrl', function($scope, Modal) {
     $scope.loan = Modal.params().loan;
-    $scope.cancel = Modal.reject;
 
     $scope.submit = function() {
       var loan = { loan: $scope.loan};
--- test/spec/controllers/modal_spec.js
+++ test/spec/controllers/modal_spec.js
@@ -1,28 +1,74 @@
 'use strict';
 
-describe('ModalCtrl', function () {
+describe('Modal Controllers', function () {
 
-  var controller, scope, Modal;
+  var controller, scope, modal;
 
   beforeEach(module('libraryUiApp'));
 
-  beforeEach(inject(function (_$controller_, _$rootScope_) {
-    Modal = {
-      params: function () {
-        return { copy: 'somecopy'};
-      },
-
-      reject: 'whatever'
-    };
-
+  beforeEach(inject(function (_$controller_, _$rootScope_, Modal) {
+    controller = _$controller_;
     scope = _$rootScope_;
-    controller = _$controller_('BorrowModalCtrl', {'$scope': scope, 'Modal': Modal});
+    modal = Modal;
   }));
 
-  describe('initialization', function () {
+
+  describe('ModalCtrl', function () {
+    describe('#cancel', function() {
+      it('rejects modal', function () {
+        controller('ModalCtrl', {'$scope': scope, 'Modal': modal});
+
+        spyOn(modal, 'reject');
+
+        scope.cancel();
+
+        expect(modal.reject).toHaveBeenCalled();
+      });
+    });
+  });  
+
+  describe('BorrowModalCtrl', function () {
     it('initizes scope correctly', function () {
-      expect(scope.copy).toBe('somecopy');
-      expect(scope.cancel).toBe('whatever');
+      controller('BorrowModalCtrl', {'$scope': scope, 'Modal': modal});
+
+      expect(scope.copy).toBe(modal.params().copy);
+    });
+
+    describe('#submit', function() {
+      it('resolves modal with loan', function () {
+        controller('BorrowModalCtrl', {'$scope': scope, 'Modal': modal});
+
+        scope.copy = '/path/to/copy/3';
+        scope.form = { 'user': 'fakeuser' };
+
+        spyOn(modal, 'resolve');
+
+        scope.submit();
+
+        expect(modal.resolve).toHaveBeenCalledWith({copy: '/path/to/copy/3', email: 'fakeuser@thoughtworks.com' });
+      });
+    });
+  });
+
+  describe('ReturnModalCtrl', function () {
+    it('initizes scope correctly', function () {
+      controller('ReturnModalCtrl', {'$scope': scope, 'Modal': modal});
+
+      expect(scope.loan).toBe(modal.params().loan);
+    });
+
+    describe('#submit', function() {
+      it('resolves modal with loan', function () {
+        controller('ReturnModalCtrl', {'$scope': scope, 'Modal': modal});
+
+        scope.loan = { 'id' : '34' }
+
+        spyOn(modal, 'resolve');
+
+        scope.submit();
+
+        expect(modal.resolve).toHaveBeenCalledWith({ 'loan' : { 'id' : '34' }});
+      });
     });
   });
 });
