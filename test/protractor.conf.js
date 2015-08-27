90ba151f39e60a119c31b719f7726e0e577b1d7e
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2015-08-27 20:27:03 -0300

    [#47] Only sets selenium driver when environment is not snap-ci

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,9 +1,10 @@
 exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['spec/features/*_spec.js']
 };
 
 if (process.env.SNAP_CI) {
   exports.config.chromeDriver = '/usr/local/bin/chromedriver';
   exports.config.chromeOnly = true;
+} else {
+  exports.config.seleniumAddress = 'http://localhost:4444/wd/hub';
 }
