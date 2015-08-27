d3742d9deac84f332b24668fd50ba759eff123de
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2015-08-27 19:24:11 -0300

    [#47] Sets up protractor for feature tests

---
+++ test/protractor.conf.js
@@ -0,0 +1,8 @@
+exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['spec/features/*_spec.js']
+};
+
+if (process.env.SNAP_CI) {
+  exports.config.chromeDriver = '/usr/local/bin/chromedriver';
+}
