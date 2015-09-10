5269d325273565659f4772335ae9a689ef15f68a
Luz <luzma_87@yahoo.com>
2015-09-10 14:44:56 -0500

    bug fix: fixed #46: show which page is open

--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -2,40 +2,43 @@
 
 angular
   .module('libraryUiApp')
-  .controller('SettingsCtrl', ['$scope', '$translate',
-    function ($scope ,$translate) {
+  .controller('SettingsCtrl', ['$scope',
+    'NavigationService',
+    '$translate',
+    function ($scope, NavigationService, $translate) {
 
-    var $cookies;
-    $scope.languages = [
-      {name:"Español Ecuador" , code : "es-EC" },
-      {name:"Portugués Brasil" , code : "pt-BR" },
-      {name:"English USA" , code : "en-US" }
+      var $cookies;
+      $scope.languages = [
+        {name : "Español Ecuador", code : "es-EC"},
+        {name : "Portugués Brasil", code : "pt-BR"},
+        {name : "English USA", code : "en-US"}
       ];
 
       $scope.goBack = function() {
-        window.history.back();
+        NavigationService.goBack();
       };
-    $scope.selectedIndex = 0;
-    angular.injector(['ngCookies']).invoke(['$cookies', function (_$cookies_) {
-      $cookies = _$cookies_;
-    }]);
-    var selectedCode = $cookies.get('language');
 
-    for (var index = 0; index < $scope.languages.length; index++) {
-      if($scope.languages[index].code == selectedCode){
-        $scope.selectedIndex = index;
-        break;
+      $scope.selectedIndex = 0;
+      angular.injector(['ngCookies']).invoke(['$cookies', function (_$cookies_) {
+        $cookies = _$cookies_;
+      }]);
+      var selectedCode = $cookies.get('language');
+
+      for (var index = 0; index < $scope.languages.length; index++) {
+        if ($scope.languages[index].code == selectedCode) {
+          $scope.selectedIndex = index;
+          break;
+        }
       }
-    }
 
-    $scope.changeLanguage = function ($index) {
-      $scope.selectedIndex = $index;
+      $scope.changeLanguage = function ($index) {
+        $scope.selectedIndex = $index;
 
-      var expireDate = new Date();
-      expireDate.setDate(expireDate.getDate() + 365);
+        var expireDate = new Date();
+        expireDate.setDate(expireDate.getDate() + 365);
 
-      var language = $scope.languages[$index].code;
-      $cookies.put('language', language, {'expires': expireDate});
-      $translate.use(language);
-	  };
-  }]);
+        var language = $scope.languages[$index].code;
+        $cookies.put('language', language, {'expires' : expireDate});
+        $translate.use(language);
+      };
+    }]);
