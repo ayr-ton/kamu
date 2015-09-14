969eb3b12c5eb99d655352e6adb6d8fd775e92f3
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2015-09-14 16:01:26 -0300

    Add okta configuration files and modify project structure

--- app.js
+++ app.js
@@ -1,29 +1,50 @@
 var express = require('express');
+var connect = require('connect');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var favicon = require('serve-favicon');
+var session = require('express-session');
+var auth = require('./auth/passport');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(connect.compress());
+app.use(session({
+  secret: "won't tell because it's secret",
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(auth.initialize());
+app.use(auth.session());
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+// view engine setup
+app.engine('html', require('ejs').renderFile);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
+// apllication routes
+app.get('/', auth.protected, function (req, res){
+  console.log(res);
+  res.render('index', { title: 'Express' });
+});
+
+app.post('/login/callback', auth.authenticate('saml', { failureRedirect: '/', failureFlash: true }), function (req, res) {
+    res.redirect('/');
+  }
+);
+
+app.get('/login', auth.authenticate('saml', { failureRedirect: '/', failureFlash: true }), function (req, res) {
+    res.redirect('/');
+  }
+);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
