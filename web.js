c135c6e5ee308d951a32b9242e6f80fa6d7213e0
Adriano Lisboa Corrêa <adrianolic@gmail.com>
2015-09-17 13:38:38 -0300

    Change server start configuration

--- web.js
+++ web.js
@@ -1,9 +1,64 @@
-var compression = require('compression')();
-var express = require('express');
-var morgan = require('morgan');
-var app = express();
+#!/usr/bin/env node
 
-app.use(morgan('dev'));
-app.use(compression);
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 5000);
+var app = require('./app');
+var debug = require('debug')('library-ui:server');
+var http = require('http');
+
+var port = normalizePort(process.env.PORT || '9000');
+app.set('port', port);
+
+var server = http.createServer(app);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
