13565dd9ae811d1c73432a6882966984f78c0014
Luz <luzma_87@yahoo.com>
2015-09-17 14:43:29 -0500

    fixed slide menu

---
+++ app/scripts/controllers/slidingMenu.js
@@ -0,0 +1,27 @@
+/**
+ * Created by lmunda on 9/17/15.
+ */
+
+angular
+  .module('libraryUiApp')
+  .controller("slidingMenu", function ($scope, $rootScope) {
+    $scope.menuVisible = false;
+
+    $scope.closeMenu = function () {
+      $scope.menuVisible = false;
+    };
+
+    $scope.showMenu = function (e) {
+      $scope.menuVisible = !$scope.menuVisible;
+      e.stopPropagation();
+    };
+
+    function _close() {
+      $scope.$apply(function () {
+        $scope.closeMenu();
+      });
+    }
+
+    $rootScope.$on("documentClicked", _close);
+    $rootScope.$on("escapePressed", _close);
+  });
