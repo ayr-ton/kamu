644c1bc5448c89cdbf0d5129b05b3c21a6ef2cdb
Adriano Lisboa Corrêa <adrianolic@gmail.com>
2015-09-23 16:51:17 -0300

    Add user gravatar in index page

--- app/scripts/controllers/library.js
+++ app/scripts/controllers/library.js
@@ -2,7 +2,7 @@
 
 angular
   .module('libraryUiApp')
-  .controller('LibraryCtrl', ['$scope', '$http', 'LibraryService', function ($scope, $http, LibraryService) {
+  .controller('LibraryCtrl', ['$scope', '$http', 'LibraryService', 'UserService', function ($scope, $http, LibraryService, UserService) {
     $scope.libraries = {};
 
     var listLibraries = function () {
@@ -10,6 +10,10 @@ angular
         success(function (data) {
           $scope.libraries = angular.isDefined(data._embedded) ? data._embedded.libraries : {};
         });
+    };
+
+    $scope.getGravatarFromSession = function(){
+      return UserService.getGravatarFromSession();
     };
 
     listLibraries();
