ab307d001ca67db77d4a889a95e6d79764a92d92
Bruno Trecenti <btrecent@thoughtworks.com>
2015-09-25 14:56:30 -0300

    [Trecenti|Francieli] setting up data before executing tests and cleaning up after

--- test/page_objects/book_list.js
+++ test/page_objects/book_list.js
@@ -5,12 +5,6 @@ module.exports = (function () {
 
   self.books = element.all(by.repeater('copy in copies'));
 
-  function waitForModalAndAcceptIt() {
-    browser.wait(protractor.ExpectedConditions.alertIsPresent(), 1000).then(function(){
-      browser.switchTo().alert().accept();
-    });
-  }
-
   self.addBookWithISBN = function (isbn) {
     addBook = self.clickAddBook();
 
@@ -23,17 +17,11 @@ module.exports = (function () {
 
   self.borrow = function (book) {
     book.element(by.css('.books-shelf-borrow')).click();
-
-    element(by.css('.modal')).element(by.model('form.user')).sendKeys('john.doe');
-    element(by.css('.modal input[type=submit]')).click();
-
-    waitForModalAndAcceptIt();
   };
 
   self.return = function (book) {
     book.element(by.css('.books-shelf-return')).click();
     element(by.css('.modal input[type=submit]')).click();
-    waitForModalAndAcceptIt();
   };
 
   self.isBorrowed = function (book) {
--- test/spec/features/borrow_a_book_spec.js
+++ test/spec/features/borrow_a_book_spec.js
@@ -1,24 +1,59 @@
-var LibrarySelect, Login;
+var LibrarySelect, Login, request;
+
+request = require('request');
 
 LibrarySelect = require('../../page_objects/library_select.js');
 Login = require('../../page_objects/login.js');
 
 describe('a user browsing the library', function () {
-  var librarySelect, bookList, login;
+  var librarySelect, bookList, login, bookCopy;
 
-  beforeEach(function () {
+  var API_ENDPOINT = 'http://localhost:8080';
+
+  function setupLibraryWithBook(done) {
+    var library = { "name": "test", "slug": "test" };
+    var book = { "title": "test book", "author": "someone"};
+    var jsonPostRequest = request.defaults({ json: true, method: 'POST' });
+
+    jsonPostRequest({ uri: API_ENDPOINT + '/libraries', body: library }, function (err, libraryResponse) {
+      jsonPostRequest({ uri: API_ENDPOINT + '/books', body: book }, function (err, bookResponse) {
+        copy = {
+          'book': bookResponse.headers['location'],
+          'library': libraryResponse.headers['location'],
+          'status': 'AVAILABLE'
+        };
+        jsonPostRequest({ uri: API_ENDPOINT + '/copies', body: copy }, function (err, copyResponse) {
+          bookCopy = {
+            url: copyResponse.headers['location'],
+            library: libraryResponse.headers['location']
+          };
+          done();
+        });
+      });
+    });
+  }
+
+  function cleanUpLibraryAndBook(done) {
+    request({ method: 'DELETE', url: bookCopy.url }, function () {
+      request({ method: 'DELETE', url: bookCopy.library }, function () {
+        done();
+      });
+    });
+  }
+
+  beforeEach(function (done) {
     login = new Login();
     login.login('John Doe');
     librarySelect = new LibrarySelect();
+    setupLibraryWithBook(done);
   });
 
-  afterEach(function () {
-    bookList = librarySelect.visit().select('Belo Horizonte');
-    bookList.return(bookList.books.first());
+  afterEach(function (done) {
+    cleanUpLibraryAndBook(done);
   });
 
   it('should be able to borrow a book', function () {
-    bookList = librarySelect.visit().select('Belo Horizonte');
+    bookList = librarySelect.visit().select('test');
 
     var book = bookList.books.first();
 
