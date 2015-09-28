34c06e03f3e260b11b49613df56b38819cd6a4b1
Bruno Trecenti <btrecent@thoughtworks.com>
2015-09-28 16:41:13 -0300

    [Trecenti|Francieli] linted navigation.js file

--- app/scripts/services/navigation.js
+++ app/scripts/services/navigation.js
@@ -2,16 +2,16 @@
 
 angular
   .module('libraryUiApp')
-  .service('NavigationService', ["$route", function ($route) {
+  .service('NavigationService', ['$route', function ($route) {
 
-    this.settings = "views/settings/settings.html";
-    this.addBook = "views/book/add-book.html";
-    this.allBooks = "views/book/index.html";
-    this.wishlist = "views/book/wishlist.html";
-    this.borrowedBooks = "views/book/borrowed-books.html";
-    this.addWish = "views/book/add-wish.html";
+    this.settings = 'views/settings/settings.html';
+    this.addBook = 'views/book/add-book.html';
+    this.allBooks = 'views/book/index.html';
+    this.wishlist = 'views/book/wishlist.html';
+    this.borrowedBooks = 'views/book/borrowed-books.html';
+    this.addWish = 'views/book/add-wish.html';
 
-    this.bookDetails = "views/book/book-details.html";
+    this.bookDetails = 'views/book/book-details.html';
     this.currentBookList = this.allBooks;
 
     this.goBack = function () {
@@ -19,7 +19,7 @@ angular
     };
 
     this.getCurrentPage = function () {
-      var page = "";
+      var page = '';
       if ($route && $route.current) {
         page = $route.current.loadedTemplateUrl;
         this.setCurrentBookList(page);
@@ -29,48 +29,48 @@ angular
 
     this.getCurrentSelectedPage = function () {
       var page = this.getCurrentPage();
-      if (page == this.bookDetails) {
+      if (page === this.bookDetails) {
         page = this.currentBookList;
       }
       return page;
     };
 
     this.setCurrentBookList = function (page) {
-      if (page == this.allBooks) {
+      if (page === this.allBooks) {
         this.currentBookList = this.allBooks;
-      } else if (page == this.wishlist) {
+      } else if (page === this.wishlist) {
         this.currentBookList = this.wishlist;
-      } else if (page == this.borrowedBooks) {
+      } else if (page === this.borrowedBooks) {
         this.currentBookList = this.borrowedBooks;
       }
     };
 
     this.isSettingsActive = function () {
-      return this.getCurrentSelectedPage() == this.settings;
+      return this.getCurrentSelectedPage() === this.settings;
     };
 
     this.isAddBookActive = function () {
-      return this.getCurrentSelectedPage() == this.addBook;
+      return this.getCurrentSelectedPage() === this.addBook;
     };
 
     this.isAddWishActive = function () {
-      return this.getCurrentSelectedPage() == this.addWish;
+      return this.getCurrentSelectedPage() === this.addWish;
     };
 
     this.isAllBooksActive = function () {
-      return this.getCurrentSelectedPage() == this.allBooks;
+      return this.getCurrentSelectedPage() === this.allBooks;
     };
 
     this.isWishlistActive = function () {
-      return this.getCurrentSelectedPage() == this.wishlist;
+      return this.getCurrentSelectedPage() === this.wishlist;
     };
 
     this.isBorrowedBooksActive = function () {
-      return this.getCurrentSelectedPage() == this.borrowedBooks;
+      return this.getCurrentSelectedPage() === this.borrowedBooks;
     };
 
     this.isBookDetails = function () {
-      return this.getCurrentPage() == this.bookDetails;
-    }
+      return this.getCurrentPage() === this.bookDetails;
+    };
 
   }]);
