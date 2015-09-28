8e61602fcc6deccd1314a442fd5607b653bb692e
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2015-09-28 16:24:11 -0300

    [Trecenti|Francieli] linted library_select.js file

--- test/page_objects/library_select.js
+++ test/page_objects/library_select.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var BookList = require('./book_list.js');
 var config = require('../../config.js');
 
@@ -5,16 +7,15 @@ module.exports = function () {
   var self = {};
 
   self.visit = function () {
-    var appEndpoint = config.current().appEndpoint
+    var appEndpoint = config.current().appEndpoint;
     browser.get(appEndpoint);
     return self;
-  }
+  };
 
   self.select = function (name) {
-    var bookList;
 
     function byName(library) {
-      return library.getText().then(function (libraryName) { return libraryName.toLowerCase() == name.toLowerCase(); });
+      return library.getText().then(function (libraryName) { return libraryName.toLowerCase() === name.toLowerCase(); });
     }
 
     browser.driver.isElementPresent(by.css('.intro-window')).then(function () {
