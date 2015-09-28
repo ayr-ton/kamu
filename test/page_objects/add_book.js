aebd7587f6d2e8526fcea779947fce5bf80b4f5b
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2015-09-28 16:19:13 -0300

    [Trecenti|Francieli] linted add_book.js file

--- test/page_objects/add_book.js
+++ test/page_objects/add_book.js
@@ -1,14 +1,16 @@
-module.exports = (function () {
+'use strict';
+
+module.exports = function () {
   var self = {};
 
   self.search = element(by.css('.isbn-search > input[type=submit]'));
   self.add = element(by.css('.main-container-window-divider > input[type=submit]'));
 
   self.fillISBN = function (isbn) {
-    var search = element(by.css('input#isbn-search'))
-    search.click('')
+    var search = element(by.css('input#isbn-search'));
+    search.click('');
     search.sendKeys(isbn);
   };
 
   return self;
-});
+};
