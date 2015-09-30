aebe078cf7598f91fab187ce04f1e59c57f43551
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2015-09-30 17:17:38 -0300

    [Trecenti|Disrael] linted code

--- app/scripts/controllers/book-details.js
+++ app/scripts/controllers/book-details.js
@@ -3,12 +3,12 @@
 angular
   .module('libraryUiApp')
   .controller('BookDetailsController', ['$scope', '$routeParams', 'BookService', 'UserService', function ($scope, $routeParams, BookService, UserService) {
-    $scope.currentBook;
+    $scope.currentBook = {};
 
     BookService.getCopy($routeParams.bookId).success(function (response) {
       $scope.currentBook = response;
 
-      if ($scope.currentBook.hasOwnProperty('lastLoan') && $scope.currentBook.lastLoan != null) {
+      if ($scope.currentBook.hasOwnProperty('lastLoan') && $scope.currentBook.lastLoan !== null) {
           $scope.currentBook.lastLoan.user = {};
           $scope.currentBook.lastLoan.user.imageUrl = UserService.getGravatarFromUserEmail($scope.currentBook.lastLoan.email);
       }
