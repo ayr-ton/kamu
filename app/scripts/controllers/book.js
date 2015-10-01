262e6e813c9b43f9b4f471c41f633891c06dcf33
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2015-10-01 11:45:49 -0300

    [Trecenti|Fantunes|Disrael] borrowCopy moethod moved from the controller to the new directive action buttons

--- app/scripts/controllers/book.js
+++ app/scripts/controllers/book.js
@@ -86,41 +86,7 @@ angular
         $scope.listBooks();
       });
 
-      $scope.borrowCopy = function (copy) {
-        var currentUser = window.sessionStorage.email;
-
-        LoanService.
-          borrowCopy(copy.id, currentUser).
-          success(function () {
-            BookService.getCopy(copy.id)
-              .success(function (data) {
-                var scope = angular.element('#copy-'.concat(copy.id)).scope();
-                scope.copy = data;
-                scope.copy.imageUrl = BookService.resolveBookImage(scope.copy.imageUrl);
-
-                scope.copy.lastLoan.user = {
-                  imageUrl: UserService.getGravatarFromUserEmail(scope.copy.lastLoan.email)
-                };
-              });
-              toastr.success('Book has been loaned to '.concat(currentUser).concat('.'));
-          }).
-          error(function (data, status) {
-            var errorMessage;
-
-            switch (status) {
-              case 412:
-                errorMessage = $translate.instant('HTTP_CODE_412');
-                break;
-              case 409:
-                errorMessage = $translate.instant('HTTP_CODE_409');
-                break;
-              default:
-                errorMessage = $translate.instant('HTTP_CODE_500');
-                break;
-            }
-            toastr.error(errorMessage);
-          });
-      };
+      
 
       $scope.returnCopy = function (copy) {
         var scope = angular.element('#modal-div').scope();
