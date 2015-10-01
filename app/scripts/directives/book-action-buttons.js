70e239fd858c4bdfab110c2aa200a797ceed10cc
Bruno Trecenti <btrecent@thoughtworks.com>
2015-10-01 11:45:49 -0300

    [Trecenti|Fantunes|Disrael] borrowCopy moethod moved from the controller to the new directive action buttons

--- app/scripts/directives/book-action-buttons.js
+++ app/scripts/directives/book-action-buttons.js
@@ -2,23 +2,64 @@
 
 angular
   .module('libraryUiApp')
-  .directive('bookActionButtons', function() {
-    function actionButtonsController ($scope) {
+  .directive('bookActionButtons', [
+    '$translate',
+    'LoanService',
+    'BookService',
+    'UserService',
+    'toastr',
+    function($translate, LoanService, BookService, UserService, toastr) {
+      function actionButtonsController ($scope) {
 
-      $scope.borrowerIsCurrentUser = function (copy) {
-        if (copy.lastLoan !== undefined && copy.lastLoan !== null) {
-          return copy.lastLoan.email.toLowerCase() === window.sessionStorage['email'].toLowerCase();
-        } else {
-          return false;
-        }
+        $scope.borrowerIsCurrentUser = function (copy) {
+          if (copy.lastLoan !== undefined && copy.lastLoan !== null) {
+            return copy.lastLoan.email.toLowerCase() === window.sessionStorage['email'].toLowerCase();
+          } else {
+            return false;
+          }
+        };
+
+        $scope.borrowCopy = function (copy) {
+          var currentUser = window.sessionStorage.email;
+
+          LoanService.
+            borrowCopy(copy.id, currentUser).
+            success(function () {
+              BookService.getCopy(copy.id)
+                .success(function (data) {
+                  $scope.copy = data;
+                  $scope.copy.imageUrl = BookService.resolveBookImage($scope.copy.imageUrl);
+
+                  $scope.copy.lastLoan.user = {
+                    imageUrl: UserService.getGravatarFromUserEmail($scope.copy.lastLoan.email)
+                  };
+                });
+                toastr.success('Book has been loaned to '.concat(currentUser).concat('.'));
+            }).
+            error(function (data, status) {
+              var errorMessage;
+
+              switch (status) {
+                case 412:
+                  errorMessage = $translate.instant('HTTP_CODE_412');
+                  break;
+                case 409:
+                  errorMessage = $translate.instant('HTTP_CODE_409');
+                  break;
+                default:
+                  errorMessage = $translate.instant('HTTP_CODE_500');
+                  break;
+              }
+              toastr.error(errorMessage);
+            });
+        };
+      }
+
+      return {
+        restrict: 'E',
+        scope: { copy: '=' },
+        replace: true,
+        controller: actionButtonsController,
+        templateUrl: 'templates/book-action-buttons.html'
       };
-    }
-
-    return {
-      restrict: 'E',
-      scope: { copy: '=' },
-      replace: true,
-      controller: actionButtonsController,
-      templateUrl: 'templates/book-action-buttons.html'
-    };
-  });
+  }]);
--- test/spec/directives/book_action_buttons_spec.js
+++ test/spec/directives/book_action_buttons_spec.js
@@ -1,10 +1,15 @@
 
 describe('Book Action Buttons Directive', function () {
-  var scope, compile, actionButtons;
+  var scope, compile, actionButtons, loanService, toastrLocal, httpBackend, apiEndpoint;
 
   beforeEach(module('libraryUiApp'));
 
-  beforeEach(inject(function ($compile, $rootScope, $templateCache) {
+  beforeEach(inject(function ($compile, $rootScope, $templateCache, $httpBackend, LoanService, toastr, ENV) {
+    loanService = LoanService;
+    toastrLocal = toastr;
+    httpBackend = $httpBackend;
+    apiEndpoint = ENV.apiEndpoint;
+
     $templateCache.put('templates/book-action-buttons.html', window.__html__['app/templates/book-action-buttons.html']);
 
     scope = $rootScope.$new();
@@ -16,6 +21,7 @@ describe('Book Action Buttons Directive', function () {
     var element = angular.element('<book-action-buttons copy="copy" ></book-action-buttons>');
     var compiled = compile(element)(scope);
     scope.$digest();
+    scope = element.isolateScope() || element.scope();
     return compiled;
   }
 
@@ -67,6 +73,63 @@ describe('Book Action Buttons Directive', function () {
       actionButtons = compileElement(copy);
 
       expect(actionButtons.find('.books-shelf-return').length).toBe(1);
+    });
+  });
+
+  describe('when borrowing a book', function() {
+    var currentUser, copy, user, loan, copyAfterBorrow;
+
+    beforeEach(function () {
+      currentUser = 'fakeuser@someemail.com';
+      copy = { 'id': '21', 'imageUrl': 'path/to/image' };
+      user = { 'imageUrl' : 'http://www.gravatar.com/avatar/1dbd3e934b5d9a64f15826f7e9e23e16' };
+      loan = {'id': '12', 'email': currentUser, 'copy': copy, 'user': user };
+      copyAfterBorrow = { 'id': '21', 'imageUrl': 'path/to/image', 'lastLoan': loan};
+    });
+
+    it('successfully borrows a book', inject(function ($window) {
+      $window.sessionStorage.email = currentUser;
+      compileElement(copy);
+
+      spyOn(loanService, 'borrowCopy').andCallThrough();
+      spyOn(toastrLocal, 'success');
+
+      httpBackend.expectPOST(apiEndpoint.concat('/loans')).respond(200);
+      httpBackend.expectGET(apiEndpoint
+        .concat('/copies/')
+        .concat(copy.id).concat('?projection=copyWithBookInline')).respond(200, copyAfterBorrow);
+
+      scope.borrowCopy(copy);
+
+      httpBackend.flush();
+
+      expect(loanService.borrowCopy).toHaveBeenCalledWith('21', 'fakeuser@someemail.com');
+      expect(scope.copy).toEqual(copyAfterBorrow);
+      expect(toastrLocal.success).toHaveBeenCalled();
+    }));
+
+    describe('copy borrow failure', function () {
+      var codes =
+        [{ 'responseCode': 412, 'errorCode': 'HTTP_CODE_412' },
+        { 'responseCode': 409, 'errorCode': 'HTTP_CODE_409' },
+        { 'responseCode': 500, 'errorCode': 'HTTP_CODE_500' }];
+
+      codes.forEach(function(item) {
+        it('shows error message', inject(function  ($window, $translate) {
+          spyOn(toastrLocal, 'error');
+          spyOn($translate, 'instant');
+          compileElement(copy);
+
+          httpBackend.expectPOST(apiEndpoint.concat('/loans')).respond(item.responseCode);
+
+          scope.borrowCopy(copy);
+
+          httpBackend.flush();
+
+          expect(toastrLocal.error).toHaveBeenCalled();
+          expect($translate.instant).toHaveBeenCalledWith(item.errorCode);
+        }));
+      });
     });
   });
 });
