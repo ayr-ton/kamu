7c4e71217b36fd5fbf92e5ed5c7af7e7161d2298
Dawson Freitas Israel <dawsonfi@gmail.com>
2015-10-05 15:15:39 -0300

    [Disrael|Fantunes] Linting code.

--- test/test-node/base.js
+++ test/test-node/base.js
@@ -3,4 +3,4 @@ var chai = require('chai');
 global.expect = chai.expect;
 
 //Testing API endpoints
-process.env.API_ENDPOINT = 'http://localhost:9000/'
+process.env.API_ENDPOINT = 'http://localhost:9000/';
