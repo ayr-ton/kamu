80c6716e4eb74b16e0a83f0114069d242aca35d7
Bruno Trecenti <btrecent@thoughtworks.com>
2015-10-06 13:00:59 -0300

    fixed files references for requires

--- test/acceptance/page_objects/login.js
+++ test/acceptance/page_objects/login.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var config = require('../../config.js');
+var config = require('../../../config.js');
 
 module.exports = function () {
   var self = {};
--- test/acceptance/utils/test_data.js
+++ test/acceptance/utils/test_data.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var request = require('request');
-var config = require('../../config.js');
+var config = require('../../../config.js');
 
 var API_ENDPOINT = config.current().apiEndpoint;
 
