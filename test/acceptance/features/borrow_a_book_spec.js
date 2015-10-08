e6fcf9845ae3b68986ea3a0a8332f28fea2eb3c6
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2015-10-08 16:53:52 -0300

    [Dawson|Tulio] Adding token on header for acceptance tests

--- test/acceptance/features/borrow_a_book_spec.js
+++ test/acceptance/features/borrow_a_book_spec.js
@@ -5,26 +5,36 @@ Login = require('../page_objects/login');
 testData = require('../utils/test_data');
 
 describe('a user browsing the library', function () {
-  var librarySelect, bookList, login;
+  var librarySelect, bookList, login, JAVASCRIPT_GOOD_PARTS_ISBN;
 
   beforeEach(function (done) {
     login = new Login();
     login.login('John Doe');
     librarySelect = new LibrarySelect();
-    testData.setupLibrary(function (err, response) {
-      testData.setupBook(response.headers.location, done);
-    });
+    JAVASCRIPT_GOOD_PARTS_ISBN = '9780596554873';
+    testData.setupLibrary(done);
   });
 
   afterEach(function (done) {
-    testData.cleanUpCopies(function () {
-      testData.cleanUpLibrary(done);
+    function byName(name) {
+      return function (copy) {
+        return copy.element(by.css('.book-shelf-title')).getText(function () {
+          return text == name;
+        });
+      }
+    }
+    bookList.books.filter(byName('JavaScript: The Good Parts')).get(0).getAttribute('id').then(function (id) {
+      var id = id.split('-')[1];
+
+      testData.deleteCopy(id, function () {
+        testData.cleanUpLibrary(done);
+      });
     });
   });
 
   it('should be able to borrow a book', function () {
     bookList = librarySelect.visit().select('test');
-
+    bookList.addBookWithISBN(JAVASCRIPT_GOOD_PARTS_ISBN);
     var book = bookList.books.first();
 
     bookList.borrow(book);
