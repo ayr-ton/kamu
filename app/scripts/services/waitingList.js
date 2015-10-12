71684474f2870bcb00b0358bf81ae6accc51ff33
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2015-10-12 11:54:17 -0500

    [Diego/David] #50 - Story finished, refactoring next

---
+++ app/scripts/services/waitingList.js
@@ -0,0 +1,16 @@
+angular.module('libraryUiApp')
+ .service('WaitingListService', ['$http', 'ENV', function ($http, ENV) {
+
+    this.getWaitingList = function () {
+      return $http.get(ENV.apiEndpoint + '/waitingLists');
+    };
+
+    this.getWaitingList = function (bookId, libraryId) {
+          return $http.get(ENV.apiEndpoint + '/waitingLists/search/findByBookAndLibrary?book_id='+bookId+'&slug='+libraryId);
+        };
+
+
+        this.getDataFromURL = function (userURL) {
+              return $http.get(userURL);
+            };
+  }]);
