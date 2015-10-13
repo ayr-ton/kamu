48202b28dbb1816ff0fad9bc560f9625c9c41ad8
Bruno Trecenti <btrecent@thoughtworks.com>
2015-10-13 15:53:15 -0300

    fixed indent style

--- app/scripts/services/waitingList.js
+++ app/scripts/services/waitingList.js
@@ -1,18 +1,17 @@
 'use strict';
 
 angular.module('libraryUiApp')
- .service('WaitingListService', ['$http', 'ENV', function ($http, ENV) {
+.service('WaitingListService', ['$http', 'ENV', function ($http, ENV) {
 
-    this.getWaitingList = function () {
-      return $http.get(ENV.apiEndpoint + '/waitingLists');
-    };
+  this.getWaitingList = function () {
+    return $http.get(ENV.apiEndpoint + '/waitingLists');
+  };
 
-    this.getWaitingList = function (bookId, libraryId) {
-          return $http.get(ENV.apiEndpoint + '/waitingLists/search/findByBookAndLibrary?book_id='+bookId+'&slug='+libraryId);
-        };
+  this.getWaitingList = function (bookId, libraryId) {
+    return $http.get(ENV.apiEndpoint + '/waitingLists/search/findByBookAndLibrary?book_id='+bookId+'&slug='+libraryId);
+  };
 
-
-        this.getDataFromURL = function (userURL) {
-              return $http.get(userURL);
-            };
-  }]);
+  this.getDataFromURL = function (userURL) {
+    return $http.get(userURL);
+  };
+}]);
