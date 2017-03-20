a6201115ad3c104b5981c1ab5cf0b98c8115b3f6
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2017-03-20 19:09:08 -0300

    :sparkles: Add Book model
    Signed-off-by: Mario Cecchi <macecchi@gmail.com>

--- books/models.py
+++ books/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    author = models.CharField(max_length = 255)
+    title = models.CharField(max_length = 255)
+    subtitle = models.CharField(max_length = 255)
+    description = models.TextField()
+    image_url = models.CharField(max_length = 255)
+    isbn = models.CharField(max_length = 255)
+    number_of_pages = models.IntegerField()
+    publication_date = models.DateField()
+    publisher = models.CharField(max_length = 255)
+
+    def __str__(self):
+        return self.title
