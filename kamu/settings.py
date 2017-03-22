87c23995d821d0d552444806efca567e55bea6a1
Mario Cecchi <macecchi@gmail.com>
2017-03-22 11:09:36 -0300

    :sparkles: Integrate boilerplate React project

--- kamu/settings.py
+++ kamu/settings.py
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/static')
+]
