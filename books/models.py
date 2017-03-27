7b4a4af227efce3261cecd7bc5ae22b445b3b48b
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2017-03-27 12:02:33 -0300

    Make Book fields optional and fix migration script

--- books/models.py
+++ books/models.py
@@ -5,13 +5,13 @@ from django.db import models
 class Book(models.Model):
     author = models.CharField(max_length = 255)
     title = models.CharField(max_length = 255)
-    subtitle = models.CharField(max_length = 255)
-    description = models.TextField()
-    image_url = models.CharField(max_length = 255)
-    isbn = models.CharField(max_length = 255)
-    number_of_pages = models.IntegerField()
-    publication_date = models.DateField()
-    publisher = models.CharField(max_length = 255)
+    subtitle = models.CharField(max_length = 255, null=True)
+    description = models.TextField(null=True)
+    image_url = models.CharField(max_length = 255, null=True)
+    isbn = models.CharField(max_length = 255, null=True)
+    number_of_pages = models.IntegerField(null=True)
+    publication_date = models.DateField(null=True)
+    publisher = models.CharField(max_length = 255, null=True)
 
     def __str__(self):
         return self.title
--- migrate.py
+++ migrate.py
@@ -1,22 +1,40 @@
 #!/usr/bin/env python
 import psycopg2
 import psycopg2.extras
-from kamu import settings
+from dateutil.parser import parse
+from books.models import Book
 
 try:
     conn=psycopg2.connect("dbname='kamu_old'")
 except:
-    print("I am unable to connect to the database.")
+    print('Unable to connect to the database.')
 
 cur = conn.cursor(cursor_factory = psycopg2.extras.DictCursor)
-try:
-    cur.execute("""SELECT * from book""")
-except:
-    print("I can't SELECT from book")
+cur.execute("SELECT * from book")
 
-rows = cur.fetchall()
-print("\nRows: \n")
-for row in rows:
-    print(row['title'])
+books = cur.fetchall()
+total_books = cur.rowcount
+imported_books = 0
 
-dir(settings)
+for book in books:
+    print('Importing ' + book['title'])
+    if book['publicationdate']:
+        try:
+            publication_date = parse(book['publicationdate'])
+        except Exception:
+            print('Publication date could not be parsed (' + book['publicationdate'] + ')')
+    
+    new_book = Book.objects.update_or_create(
+        author=book['author'],
+        title=book['title'],
+        subtitle=book['subtitle'],
+        description=book['description'],
+        image_url=book['imageurl'],
+        isbn=book['isbn'],
+        number_of_pages=book['numberofpages'],
+        publication_date=publication_date,
+        publisher=book['publisher'],
+    )
+    imported_books += 1
+
+print("Imported %d books of %d." % (imported_books, total_books))
