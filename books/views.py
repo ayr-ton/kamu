81275658a776637620303130c61b3ac2e9b077b7
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2017-03-28 15:47:34 -0300

    Return books only in the library’s retrieve view

--- books/views.py
+++ books/views.py
@@ -1,4 +1,6 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from rest_framework.response import Response
 from books.serializers import *
 from books.models import *
 
@@ -9,4 +11,10 @@ class BookViewSet(viewsets.ModelViewSet):
 
 class LibraryViewSet(viewsets.ModelViewSet):
     queryset = Library.objects.all()
-    serializer_class = LibrarySerializer
+    serializer_class = LibraryCompactSerializer
+
+    def retrieve(self, request, pk=None):
+        queryset = Library.objects.all()
+        library = get_object_or_404(queryset, pk=pk)
+        serializer = LibrarySerializer(library, context={ 'request': request })
+        return Response(serializer.data)
