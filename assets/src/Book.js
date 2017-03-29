fd2ad7883e40c7a52bea149afc221b6d53f853b5
Mario Cecchi <macecchi@gmail.com>
2017-03-29 10:56:52 -0300

    Improve design of the book list

--- assets/src/Book.js
+++ assets/src/Book.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
-import {Card, CardMedia, CardTitle} from 'material-ui/Card';
+import Paper from 'material-ui/Paper';
 
 export default class Book extends Component {
+	constructor() {
+		super();
+		this.state = { zDepth: 1 };
+	}
+
+	onMouseOver = () => this.setState({ zDepth: 2 });
+	onMouseOut = () => this.setState({ zDepth: 1 });
+
 	render() {
 		const book = this.props.book;
-		return (
-			<Card className="book">
-				<CardMedia>
-					<img src={book.image_url} alt={"Cover of " + book.title} />
-				</CardMedia>
 
-				<CardTitle title={book.title} subtitle={book.author} />
-			</Card>
+		return (
+			<Paper className="book" zDepth={this.state.zDepth} onMouseOver={this.onMouseOver} onMouseOut={this.onMouseOut}>
+				<img className="book-cover" src={book.image_url} alt={"Cover of " + book.title} />
+				<div className="book-details">
+					<h1 className="book-title">{book.title}</h1>
+					<h2 className="book-author">{book.author}</h2>
+				</div>
+			</Paper>
 		);
 	}
 }
