e23b845e3fb984f4e4ad7a6cf6123e7f3ac5b606
Ayrton Araujo <aaraujo@thoughtworks.com>
2017-04-04 17:43:16 -0300

    :hankey: Testing CSS imports

--- client/assets/src/Header.js
+++ client/assets/src/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import '../css/App.css'
 
 export default class Header extends Component {
 	render() {
