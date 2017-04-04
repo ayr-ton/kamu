eaf01a74a8bad78ea72b60e6743fe6153043c7ed
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2017-04-04 17:46:42 -0300

    :art: Refactored CSS import

--- client/assets/src/index.js
+++ client/assets/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import Header from './Header';
 import '../css/index.css';
+import '../css/App.css'
 
 injectTapEventPlugin();
 
