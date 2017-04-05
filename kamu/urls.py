46b21057d583817fa0dafe19189e5b39d90336da
Ayrton Araujo <aaraujo@thoughtworks.com>
2017-04-05 15:38:26 -0300

    :sparkles: Okta integration is finished with adjustments

--- kamu/urls.py
+++ kamu/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 from rest_framework import routers
+from django.contrib.auth.decorators import login_required
 import django_saml2_auth.views
 from books import views
 
@@ -11,8 +12,9 @@ router.register(r'books', views.BookViewSet)
 router.register(r'libraries', views.LibraryViewSet)
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='home.html')),
-    url(r'^libraries/(?P<slug>.+)/', TemplateView.as_view(template_name='libraries.html')),
+    url(r'^$', login_required(TemplateView.as_view(template_name='home.html'))),
+    url(r'^libraries/(?P<slug>.+)/', login_required(TemplateView.as_view(template_name='libraries.html'))),
+    url(r'^accounts/login', django_saml2_auth.views.signin),
     url(r'^okta-login/', include('django_saml2_auth.urls')),
     url(r'^admin/login/$', django_saml2_auth.views.signin),
     url(r'^admin/', admin.site.urls),
