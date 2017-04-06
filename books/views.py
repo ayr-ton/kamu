14391493e2f6c823b019dfdde35b2c965085629a
Ayrton Araujo <aaraujo@thoughtworks.com>
2017-04-05 23:35:32 -0300

    Updated dependencies and removed unecessary imports

--- books/views.py
+++ books/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 from hashlib import md5
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response    
 from rest_framework.views import APIView
 from rest_framework import viewsets
