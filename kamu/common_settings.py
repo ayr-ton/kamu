01025de1aa3da80fd200b1a715dc362c251d4b07
Ayrton Araujo <aaraujo@thoughtworks.com>
2017-04-06 02:19:38 -0300

    Adding public folder generated from webpack to staticfiles

--- kamu/common_settings.py
+++ kamu/common_settings.py
@@ -70,7 +70,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'assets/'),
+    os.path.join(BASE_DIR, 'assets'),
+    os.path.join(BASE_DIR, 'public')
 ]
 
 WEBPACK_LOADER = {
