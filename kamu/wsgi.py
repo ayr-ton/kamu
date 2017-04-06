37151da8ea18846d0247bd12482b36dfadf1a461
Ayrton Araujo <aaraujo@thoughtworks.com>
2017-04-06 02:14:32 -0300

    whitenoise for gunicorn staticfiles support

--- kamu/wsgi.py
+++ kamu/wsgi.py
@@ -1,7 +1,7 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kamu.settings")
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
