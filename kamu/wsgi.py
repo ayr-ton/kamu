2df580190b52b465b1be63287e245922cfbf20a7
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2017-04-06 14:34:24 -0300

    :bug: Changed wsgi behaviour for okta

--- kamu/wsgi.py
+++ kamu/wsgi.py
@@ -3,5 +3,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kamu.settings")
+
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
