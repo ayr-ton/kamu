26f63e6966d1ddd4023cbb04f54e1eb6b2bdd7b5
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2017-04-20 15:23:57 -0300

    :sparkles: Add borrow button

--- assets/src/libraries/Book.js
+++ assets/src/libraries/Book.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Paper from 'material-ui/Paper';
+import FlatButton from 'material-ui/FlatButton';
 
 export default class Book extends Component {
 	constructor() {
@@ -8,10 +9,19 @@ export default class Book extends Component {
 
 		this.onMouseOver = this.onMouseOver.bind(this);
 		this.onMouseOut = this.onMouseOut.bind(this);
+		this._actionButtons = this._actionButtons.bind(this);
 	}
 
 	onMouseOver() { return this.setState({ zDepth: 2 }); }
 	onMouseOut() { this.setState({ zDepth: 1 }); }
+
+	_actionButtons() {
+		if (this.props.book.isAvailable()) {
+			return <FlatButton label="Borrow" className="borrow-button" />;
+		}
+
+		return null;
+	}
 
 	render() {
 		const book = this.props.book;
@@ -22,9 +32,14 @@ export default class Book extends Component {
 					<img src={book.image_url} alt={"Cover of " + book.title} />
 					<div className="book-cover-overlay"></div>
 				</div>
+
 				<div className="book-details">
 					<h1 className="book-title">{book.title}</h1>
 					<h2 className="book-author">{book.author}</h2>
+				</div>
+
+				<div className="book-actions">
+					{this._actionButtons()}
 				</div>
 			</Paper>
 		);
