ae562ccb4819c779de741d6b3e3ecc61a9b6e4fc
Ayrton Araujo <aaraujo@thoughtworks.com>
2017-05-18 17:21:38 -0300

    Added hidden option to disable okta for debugging porpuses

--- kamu/urls.py
+++ kamu/urls.py
@@ -6,18 +6,16 @@ from django.views.generic import TemplateView, RedirectView
 from rest_framework import routers
 from django.contrib.auth.decorators import login_required
 from books import views
+import os
 
 
 router = routers.DefaultRouter()
 router.register(r'libraries', views.LibraryViewSet)
 router.register(r'copies', views.BookCopyViewSet)
 
-urlpatterns = [
+urlpatterns = [ 
     url(r'^$', login_required(TemplateView.as_view(template_name='home.html'))),
     url(r'^libraries/(?P<slug>.+)/', login_required(TemplateView.as_view(template_name='libraries.html'))),
-    url(r'^accounts/login', django_saml2_auth.views.signin),
-    url(r'^okta-login/', include('django_saml2_auth.urls')),
-    url(r'^admin/login/$', django_saml2_auth.views.signin),
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(router.urls)),
     url(r'^api/profile', views.UserView.as_view()),
@@ -25,3 +23,10 @@ urlpatterns = [
     url(r'^api/copies/(?P<id>.+)/return', views.BookCopyReturnView.as_view()),
     url(r'^favicon\.ico$', RedirectView.as_view(url=settings.STATIC_URL + 'images/favicon.ico'))
 ]
+
+if os.environ.get("DISABLE_SAML2"):
+    urlpatterns.append(url(r'^accounts/login', admin.site.login))
+else:
+    urlpatterns.append(url(r'^accounts/login', django_saml2_auth.views.signin))
+    urlpatterns.append(url(r'^admin/login/$', django_saml2_auth.views.signin))
+    urlpatterns.append(url(r'^okta-login/', include('django_saml2_auth.urls')))
