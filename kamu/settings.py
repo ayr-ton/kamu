76e269a41b443b9a25fb0c8d5b2c519835cea726
Ayrton Araujo <aaraujo@thoughtworks.com>
2017-05-18 17:43:19 -0300

    Removed any dependency with ThoughtWorks infra

--- kamu/settings.py
+++ kamu/settings.py
@@ -14,5 +14,3 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-SAML2_AUTH['METADATA_AUTO_CONF_URL'] = 'https://thoughtworks.oktapreview.com/app/exk4v36owiuXaaDq50h7/sso/saml/metadata'
