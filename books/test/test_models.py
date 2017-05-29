e340e9224d45efa32abe4ab9bf9254f79d5f9154
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2017-05-29 15:51:28 -0400

    [JC/Clau] Added tests for models

---
+++ books/test/test_models.py
@@ -0,0 +1,85 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.utils import timezone
+
+from books.models import Book, BookCopy, Library
+
+
+# MODELS
+class BookTestCase(TestCase):
+    def test_book_creation(self):
+        self.book = Book.objects.create(author="Author", title="the title", subtitle="The subtitle",
+                                        publication_date=timezone.now())
+        self.assertEqual(self.book.author, "Author")
+        self.assertEqual(self.book.title, "the title")
+        self.assertEqual(self.book.subtitle, "The subtitle")
+
+    def test_book_type_data(self):
+        self.book = Book.objects.create(author="Author", title="the title", subtitle="The subtitle",
+                                        publication_date=timezone.now())
+        self.assertIsInstance(self.book.author, str)
+        self.assertIsInstance(self.book.title, str)
+        self.assertIsInstance(self.book.subtitle, str)
+
+
+class LibraryTestCase(TestCase):
+    def setUp(self):
+        self.book = Book.objects.create(author="Author", title="the title", subtitle="The subtitle",
+                                        publication_date=timezone.now())
+        self.book.save()
+
+        self.library = Library.objects.create(name="Santiago", slug="slug")
+        self.user = User.objects.create(username="claudia", email="claudia@gmail.com")
+
+    def test_library_creation(self):
+        self.library = Library.objects.create(name="Santiago", slug="slug")
+
+        self.assertEqual(self.library.name, "Santiago")
+        self.assertEqual(self.library.slug, "slug")
+
+    def test_verbose_name_plural(self):
+        self.assertEqual(str(Library._meta.verbose_name_plural), "libraries")
+
+    def test_library_should_have_one_book(self):
+        self.bookCopy = BookCopy.objects.create(book=self.book, library=self.library, user=self.user)
+
+        self.assertEqual(1, len(self.library.books.all()))
+
+
+class BookCopyTestCase(TestCase):
+    def setUp(self):
+        self.book = Book.objects.create(author="Author", title="the title", subtitle="The subtitle",
+                                        publication_date=timezone.now())
+        self.book.save()
+
+        self.libray = Library.objects.create(name="Santiago", slug="slug")
+        self.user = User.objects.create(username="claudia", email="claudia@gmail.com")
+
+    def test_book_copy_creation(self):
+        self.bookCopy = BookCopy.objects.create(book=self.book, library=self.libray, user=self.user)
+        self.assertEqual(self.bookCopy.book, self.book)
+        self.assertEqual(self.bookCopy.library, self.libray)
+
+    def test_verbose_name_plural(self):
+        self.bookCopy = BookCopy.objects.create(book=self.book, library=self.libray, user=self.user)
+        self.assertEquals(str(BookCopy._meta.verbose_name_plural), "Book copies")
+
+
+# VIEWS
+class UserViewTestCase(TestCase):
+    def test_user_view_get(self):
+        pass
+
+    def test_api_profile(self):
+        response = self.client.get('/api/profile', follow=True)
+        self.assertEquals(response.status_code, 403)
+
+    def test_api_copies_return(self):
+        pass
+
+    def test_api_copies_borrow(self):
+        pass
+
+    def test_api(self):
+        response = self.client.get('/api/', follow=True)
+        self.assertNotEqual(response.status_code, 200)
