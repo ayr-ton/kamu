d4801c887cbb286fb41be4f8948de642003e2c20
vreyn <aecheve@thoughtworks.com>
2017-05-29 16:59:04 -0400

    [aecheve, bsoto] Added test for borrowCopy method of BookService class

--- assets/src/services/BookService.test.js
+++ assets/src/services/BookService.test.js
@@ -116,6 +116,75 @@ describe('BookService', () => {
         });
     });
 
+    describe('Borrow book', () => {
+        let book = new Book();
+        book.id = 1;
+        book.author = "Kent Beck";
+        book.title = "Test Driven Development";
+        book.subtitle = "By Example";
+        book.desciption = "Lorem ipsum...";
+        book.image_url = "http://books.google.com.br/books/content?id=gFgnde_vwMAC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api";
+        book.isbn = "9780321146533";
+        book.number_of_pages = 220;
+        book.publication_date = "2003-05-17";
+        book.publisher = "Addison-Wesley Professional";
 
+        book.copies = [
+            {
+              "id": 1348,
+              "user": {
+                "username": "bherrera@thoughtworks.com",
+                "email": "bherrera@thoughtworks.com",
+                "image_url": "https://www.gravatar.com/avatar/5cf7021537744b09534beb1d66adfbea?size=100"
+              }
+            }
+            ,{
+              "id": 1349,
+              "user": null
+            }
+        ];
+
+        let user = {
+            username: "test@thoughtsworks.com"
+            , email: "test@thoughtsworks.com"
+            , image_url: ""
+        }
+
+
+        let sandbox;
+        beforeEach(() => {
+            sandbox = sinon.sandbox.create();
+            sandbox.stub(
+                require("./helpers")
+                , "fetchFromAPI"
+            ).withArgs(`/copies/${book.copies[1].id}/borrow`)
+            .returns(Promise.resolve({}));
+
+            sandbox.stub(
+                book
+                , "getAvailableCopyID"
+            ).returns(book.copies[1].id);
+
+            global.currentUser = user;
+        });
+
+        afterEach(() => {
+           sandbox.restore();
+        });
+
+        it("Should borrow copy", (done) => {
+            let bookService = new BookService();
+
+            bookService.borrowBook(book).then(data => {
+                expect(data).to.be.true
+                expect(book.copies[1].user).to.deep.equal(user);
+
+                done();
+            });
+        });
+
+
+        //ToDo: Add a test for the case that no copies are available, expect return False from the method
+    });
 
 });
