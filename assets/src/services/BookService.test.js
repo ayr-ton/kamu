299ee8660458cf43b33e49fc772b807d1fe7fbbd
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2017-05-31 15:01:52 -0400

    [bsoto/joanca] Refactor BookService test

--- assets/src/services/BookService.test.js
+++ assets/src/services/BookService.test.js
@@ -3,9 +3,8 @@ import { expect } from 'chai';
 import BookService from "./BookService";
 import Book from "../models/Book";
 
-describe('BookService', () => {
-    describe('Get Libraries', () => {
-        const libraries = [
+function generateLibraries() {
+    return [
             {
                 id: 1,
                 url: "http://localhost:8000/api/libraries/quito/",
@@ -19,6 +18,64 @@ describe('BookService', () => {
                 slug: "bh"
             }
         ];
+}
+
+function generateUser() {
+    return {
+        username: "test@thoughtsworks.com"
+        , email: "test@thoughtsworks.com"
+        , image_url: ""
+    };
+}
+
+function generateBooks() {
+    let books = [];
+    let book1 = new Book();
+    book1.id = 1;
+    book1.author = "Kent Beck";
+    book1.title = "Test Driven Development";
+    book1.subtitle = "By Example";
+    book1.desciption = "Lorem ipsum...";
+    book1.image_url = "http://books.google.com.br/books/content?id=gFgnde_vwMAC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api";
+    book1.isbn = "9780321146533";
+    book1.number_of_pages = 220;
+    book1.publication_date = "2003-05-17";
+    book1.publisher = "Addison-Wesley Professional";
+
+    book1.copies = [{
+          "id": 1348,
+          "user": {
+            "username": "bherrera@thoughtworks.com",
+            "email": "bherrera@thoughtworks.com",
+            "image_url": "https://www.gravatar.com/avatar/5cf7021537744b09534beb1d66adfbea?size=100"
+          }
+        }];
+    books.push(book1);
+    
+    let book2 = new Book();
+    book2.id = 2;
+    book2.author = "Robert Martin";
+    book2.title = "Clean Code";
+    book2.subtitle = "A Handbook of Agile Software Craftsmanship";
+    book2.desciption = "Lorem ipsum...";
+    book2.image_url = "http://books.google.com.br/books/content?id=gFgnde_vwMAC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api";
+    book2.isbn = "9780132350884";
+    book2.number_of_pages = 431;
+    book2.publication_date = "2009-05-17";
+    book2.publisher = "Pearson Education";
+
+    book2.copies = [{
+            "id": 1349,
+            "user": null
+        }];
+    books.push(book2);
+
+    return books;
+}
+
+describe('BookService', () => {
+    describe('Get Libraries', () => {
+        const libraries = generateLibraries();
 
         let sandbox;
         beforeEach(() => {
@@ -46,46 +103,7 @@ describe('BookService', () => {
     });
 
     describe('Get Books', () => {
-        let books = [];
-        let book1 = new Book();
-        book1.id = 1;
-        book1.author = "Kent Beck";
-        book1.title = "Test Driven Development";
-        book1.subtitle = "By Example";
-        book1.desciption = "Lorem ipsum...";
-        book1.image_url = "http://books.google.com.br/books/content?id=gFgnde_vwMAC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api";
-        book1.isbn = "9780321146533";
-        book1.number_of_pages = 220;
-        book1.publication_date = "2003-05-17";
-        book1.publisher = "Addison-Wesley Professional";
-
-        book1.copies = [{
-              "id": 1348,
-              "user": {
-                "username": "bherrera@thoughtworks.com",
-                "email": "bherrera@thoughtworks.com",
-                "image_url": "https://www.gravatar.com/avatar/5cf7021537744b09534beb1d66adfbea?size=100"
-              }
-            }];
-        books.push(book1);
-        
-        let book2 = new Book();
-        book2.id = 2;
-        book2.author = "Robert Martin";
-        book2.title = "Clean Code";
-        book2.subtitle = "A Handbook of Agile Software Craftsmanship";
-        book2.desciption = "Lorem ipsum...";
-        book2.image_url = "http://books.google.com.br/books/content?id=gFgnde_vwMAC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api";
-        book2.isbn = "9780132350884";
-        book2.number_of_pages = 431;
-        book2.publication_date = "2009-05-17";
-        book2.publisher = "Pearson Education";
-
-        book2.copies = [{
-                "id": 1349,
-                "user": null
-            }];
-        books.push(book2);
+        let books = generateBooks();
 
         const slug = "quito";
 
@@ -115,42 +133,28 @@ describe('BookService', () => {
     });
 
     describe('Borrow book', () => {
-        let book = new Book();
-        book.id = 1;
-        book.author = "Kent Beck";
-        book.title = "Test Driven Development";
-        book.subtitle = "By Example";
-        book.desciption = "Lorem ipsum...";
-        book.image_url = "http://books.google.com.br/books/content?id=gFgnde_vwMAC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api";
-        book.isbn = "9780321146533";
-        book.number_of_pages = 220;
-        book.publication_date = "2003-05-17";
-        book.publisher = "Addison-Wesley Professional";
+        let book = generateBooks()[0];
 
-        book.copies = [
-            {
-              "id": 1348,
-              "user": {
-                "username": "bherrera@thoughtworks.com",
-                "email": "bherrera@thoughtworks.com",
-                "image_url": "https://www.gravatar.com/avatar/5cf7021537744b09534beb1d66adfbea?size=100"
-              }
-            }
-            ,{
-              "id": 1349,
-              "user": null
-            }
-        ];
-
-        let user = {
-            username: "test@thoughtsworks.com"
-            , email: "test@thoughtsworks.com"
-            , image_url: ""
-        }
+        let user = generateUser();
 
 
         let sandbox;
         beforeEach(() => {
+            book.copies = [
+                {
+                  "id": 1348,
+                  "user": {
+                    "username": "bherrera@thoughtworks.com",
+                    "email": "bherrera@thoughtworks.com",
+                    "image_url": "https://www.gravatar.com/avatar/5cf7021537744b09534beb1d66adfbea?size=100"
+                  }
+                }
+                ,{
+                  "id": 1349,
+                  "user": null
+                }
+            ];
+
             sandbox = sinon.sandbox.create();
             sandbox.stub(
                 require("./helpers")
@@ -184,42 +188,26 @@ describe('BookService', () => {
     });
 
     describe('Return book', () => {
-        let book = new Book();
-        book.id = 1;
-        book.author = "Kent Beck";
-        book.title = "Test Driven Development";
-        book.subtitle = "By Example";
-        book.desciption = "Lorem ipsum...";
-        book.image_url = "http://books.google.com.br/books/content?id=gFgnde_vwMAC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api";
-        book.isbn = "9780321146533";
-        book.number_of_pages = 220;
-        book.publication_date = "2003-05-17";
-        book.publisher = "Addison-Wesley Professional";
-
-        let user = {
-            username: "test@thoughtsworks.com"
-            , email: "test@thoughtsworks.com"
-            , image_url: ""
-        };
-
-        book.copies = [
-            {
-              "id": 1348,
-              "user": {
-                "username": "bherrera@thoughtworks.com",
-                "email": "bherrera@thoughtworks.com",
-                "image_url": "https://www.gravatar.com/avatar/5cf7021537744b09534beb1d66adfbea?size=100"
-              }
-            }
-            ,{
-              "id": 1349,
-              "user": user
-            }
-        ];
-
+        let book = generateBooks()[0];
+        let user = generateUser();
 
         let sandbox;
         beforeEach(() => {
+            book.copies = [
+                {
+                  "id": 1348,
+                  "user": {
+                    "username": "bherrera@thoughtworks.com",
+                    "email": "bherrera@thoughtworks.com",
+                    "image_url": "https://www.gravatar.com/avatar/5cf7021537744b09534beb1d66adfbea?size=100"
+                  }
+                }
+                ,{
+                  "id": 1349,
+                  "user": user
+                }
+            ];
+
             sandbox = sinon.sandbox.create();
             sandbox.stub(
                 require("./helpers")
