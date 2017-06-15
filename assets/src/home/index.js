c9637716e46711a76c85e145edf91ab8112e8160
Mario Cecchi <mcecchi@thoughtworks.com>
2017-06-14 22:05:17 -0300

    Redirect to saved region automatically

--- assets/src/home/index.js
+++ assets/src/home/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import LibraryRedirector from './LibraryRedirector';
 import LibrarySelector from './LibrarySelector';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import BookService from '../services/BookService';
 import ProfileService from '../services/ProfileService';
 
+const profileService = new ProfileService();
+const bookService = new BookService();
+
 ReactDOM.render((
 	<MuiThemeProvider>
-		<LibrarySelector bookService={new BookService()} profileService={new ProfileService()} />
+		<LibraryRedirector profileService={profileService}>
+			<LibrarySelector bookService={bookService} profileService={profileService} />
+		</LibraryRedirector>
 	</MuiThemeProvider>
 ), document.getElementById('home'));
