8461d7372c39e8b941c59a4cba7f846896182ce7
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2017-06-15 16:09:57 -0300

    Hide header menu in home

--- assets/src/Header.test.js
+++ assets/src/Header.test.js
@@ -1,23 +1,46 @@
-import React from 'react';
-import Header from './Header';
-import { shallow } from 'enzyme';
-import { expect } from 'chai';
-import sinon from 'sinon';
+import React from "react";
+import Header from "./Header";
+import {shallow} from "enzyme";
+import {expect} from "chai";
+import sinon from "sinon";
 
 describe('Header', () => {
-	it('should clear the region and redirect to home', () => {
-		global.window = { location: { href: '' }};
-		let profileService = {
-			getLoggedUser: () => Promise.resolve({}),
-			clearRegion: () => {}
-		};
+    let profileService;
+	let header;
 
-		let header = shallow(<Header service={profileService} />);
+    beforeEach(() => {
+		global.window = { location: { href: '' }};
+
+        profileService = {
+            getLoggedUser: () => Promise.resolve({}),
+            clearRegion: () => {
+            }
+        };
+    });
+
+    function renderHeader() {
+        header = shallow(<Header service={profileService}/>);
+    }
+
+    it('should clear the region and redirect to home', () => {
 		const clearRegion = sinon.spy(profileService, 'clearRegion');
 
+        renderHeader();
 		header.instance()._changeRegion();
 
 		expect(clearRegion.called).to.be.true;
         expect(global.window.location.href).to.equal('/');
 	});
+
+	it('should display the change library menu', () => {
+		global.window.location.pathname = '/foo';
+        renderHeader();
+        expect(header.state('displaysMenu')).to.be.true;
+	});
+
+    it('should not display the change library menu in home', () => {
+		global.window.location.pathname = '/';
+        renderHeader();
+        expect(header.state('displaysMenu')).to.be.false;
+    });
 });
