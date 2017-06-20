7caef644bad5664b283c1013e4a7dc91050956df
isaworks <isabel.villavicencio@thoughtworks.com>
2017-06-20 16:49:25 -0400

    [ivillavi/bsoto/clopez] Testing borrowings out of term

--- books/cron/send_notification.py
+++ books/cron/send_notification.py
@@ -1,2 +1,13 @@
-def send_notification():
+import datetime
+from dateutil.relativedelta import relativedelta
+
+from books.models import Book, Library, BookCopy
+
+def send_notification(bookCopyList):
     print("Hola mundo")
+    for bookCopy in bookCopyList:
+        print(bookCopy.user+" recuerda devolver "+bookCopy.book.title)
+
+def get_borrows_out_of_time(number_months_borrowed_limit):
+    limitDate = datetime.datetime.today() - relativedelta(months=number_months_borrowed_limit)
+    return list(BookCopy.objects.filter(borrow_date__lte=limitDate))
