22200af9a7b543dd68c905883cf0de2600414950
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2017-06-20 16:49:25 -0400

    [ivillavi/bsoto/clopez] Testing borrowings out of term

---
+++ books/test/test_notifications.py
@@ -0,0 +1,37 @@
+import datetime
+
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.utils import timezone
+
+from books.models import Book, Library, BookCopy
+
+from books.cron.send_notification import *
+
+class SendNotifications(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user("isabel")
+        self.book = Book.objects.create(author="Author", title="the title", subtitle="The subtitle",
+                                        publication_date=timezone.now())
+        self.library = Library.objects.create(name="Santiago", slug="slug")
+
+        self.bookCopies = {}
+        book = BookCopy.objects.create(book=self.book, library=self.library, user=self.user,
+                                                       borrow_date=datetime.date(2017, 3, 1))
+        self.bookCopies[book.id] = book
+
+        book = BookCopy.objects.create(book=self.book, library=self.library, user=self.user,
+                                       borrow_date=datetime.date(2017, 1, 1))
+        self.bookCopies[book.id] = book
+
+        book = BookCopy.objects.create(book=self.book, library=self.library, user=self.user,
+                                       borrow_date=datetime.date(2017, 6, 1))
+        self.bookCopies[book.id] = book
+
+    def test_usrs_out_of_term(self):
+        bookCopiesRet = get_borrows_out_of_time(3)
+
+        self.assertEqual(len(bookCopiesRet), 2)
+
+        for bookCopy in bookCopiesRet:
+            self.assertEqual(bookCopy, self.bookCopies[bookCopy.id])
