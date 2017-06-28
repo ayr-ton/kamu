735f64f88c47029b8f22d5d20b6711adf53ec9d9
Benjamin Soto <bsoto@thoughtworks.com>
2017-06-28 16:12:23 -0400

    added console mail testing

--- books/cron/send_notification.py
+++ books/cron/send_notification.py
@@ -1,13 +1,21 @@
 import datetime
 from dateutil.relativedelta import relativedelta
+from django.core.mail import send_mail
 
 from books.models import Book, Library, BookCopy
 
+template = "Usuario, recuerde devolver el libro"
+
 def send_notification(bookCopyList):
-    print("Hola mundo")
-    for bookCopy in bookCopyList:
-        print(bookCopy.user+" recuerda devolver "+bookCopy.book.title)
+	send_mail(
+		'Subject here',
+		'Here is the message.',
+		'from@example.com',
+		['to@example.com'],
+		fail_silently=False,
+	)
+	return True
 
 def get_borrows_out_of_time(number_months_borrowed_limit):
-    limitDate = datetime.datetime.today() - relativedelta(months=number_months_borrowed_limit)
-    return list(BookCopy.objects.filter(borrow_date__lte=limitDate))
+	limitDate = datetime.datetime.today() - relativedelta(months=number_months_borrowed_limit)
+	return list(BookCopy.objects.filter(borrow_date__lte=limitDate))
