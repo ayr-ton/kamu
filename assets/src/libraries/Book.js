4f3a6ee5ecdab0661d5aff1d67cca30927daa6bd
Adriano Godoy <agodoy@thoughtworks.com>
2017-06-29 13:03:26 -0300

    Track events on book detail click, borrow and return

--- assets/src/libraries/Book.js
+++ assets/src/libraries/Book.js
@@ -31,12 +31,14 @@ export default class Book extends Component {
 	_borrow() {
 		this.props.service.borrowBook(this.props.book).then(() => {
 			this.setState({ available: false, borrowedByMe: true });
+			window.ga('send', 'event', 'Borrow', this.props.book.title, this.props.library);
 		});
 	}
 
 	_return() {
 		this.props.service.returnBook(this.props.book).then(() => {
 			this.setState({ available: true, borrowedByMe: false });
+			window.ga('send', 'event', 'Return', this.props.book.title, this.props.library);
 		});
 	}
 
@@ -51,13 +53,12 @@ export default class Book extends Component {
 	}
 
 	changeOpenStatus() { 		
-		this.setState({ open: !this.state.open }, this._trackAnalytics);	
-				
+		this.setState({ open: !this.state.open }, this._trackAnalytics);					
 	}
 
 	_trackAnalytics() {
-		if(this.state.open) {
-			window.ga('send', 'event', 'Show Detail', this.props.book.title);
+		if(this.state.open) {														
+			window.ga('send', 'event', 'Show Detail', this.props.book.title, this.props.library);
 		}
 	}
 
@@ -68,7 +69,7 @@ export default class Book extends Component {
 		if (this.state.open) {
 			contentDetail = <BookDetail open={this.state.open} book={book} changeOpenStatus={this.changeOpenStatus}  />
 		}
-
+		
 		const bookCover ={
 			backgroundImage: `url('${book.image_url}')`
 		};
