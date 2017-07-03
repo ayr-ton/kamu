2f4bf47aa7326ab7d76fe60a3530ecb4572ce4d2
Benjamin Soto <bsoto@thoughtworks.com>
2017-07-03 15:27:09 -0400

    [ivillavi/bsoto] Finished send notification feature.

--- books/cron/send_notification.py
+++ books/cron/send_notification.py
@@ -1,20 +1,26 @@
 import datetime
 from dateutil.relativedelta import relativedelta
+
 from django.core.mail import send_mail
+from django.conf import settings
 
 from books.models import Book, Library, BookCopy
 
-template = "Usuario, recuerde devolver el libro"
 
-def send_notification(bookCopyList):
-	send_mail(
-		'Subject here',
-		'Here is the message.',
-		'from@example.com',
-		['to@example.com'],
-		fail_silently=False,
-	)
-	return True
+
+def send_borrows_out_of_time_notifications():
+	borrow_book_out_of_time = get_borrows_out_of_time(settings.CRON_EMAIL_NOTIFICATION_SETTINGS["BORROW_MAX_TERM_MONTH"])
+	send_notification(borrow_book_out_of_time)
+
+def send_notification(book_list):
+	for book_copy in book_list:
+		send_mail(
+			settings.CRON_EMAIL_NOTIFICATION_SETTINGS["TEMPLATE_SUBJECT"],
+			settings.CRON_EMAIL_NOTIFICATION_SETTINGS["TEMPLATE_BODY"].format(bookName=book_copy.book.title, borrowedDate=book_copy.borrow_date, maxTerm=settings.CRON_EMAIL_NOTIFICATION_SETTINGS["BORROW_MAX_TERM_MONTH"]),
+			settings.CRON_EMAIL_NOTIFICATION_SETTINGS["TEMPLATE_FROM"],
+			[book_copy.user],
+			fail_silently=False,
+		)
 
 def get_borrows_out_of_time(number_months_borrowed_limit):
 	limitDate = datetime.datetime.today() - relativedelta(months=number_months_borrowed_limit)
