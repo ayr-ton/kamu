1742ed89cdb13b03e734c4b41c96e2c6a4cad9be
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2017-07-03 15:27:09 -0400

    [ivillavi/bsoto] Finished send notification feature.

--- books/test/test_notifications.py
+++ books/test/test_notifications.py
@@ -1,16 +1,18 @@
 import datetime
 
+from django.core import mail
 from django.contrib.auth.models import User
 from django.test import TestCase
 from django.utils import timezone
+from django.conf import settings
 
 from books.models import Book, Library, BookCopy
 
 from books.cron.send_notification import *
 
-class SendNotifications(TestCase):
+class SendNotificationsTest(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user("isabel")
+        self.user = User.objects.create_user("test@thoughtworks.com")
         self.book = Book.objects.create(author="Author", title="the title", subtitle="The subtitle",
                                         publication_date=timezone.now())
         self.library = Library.objects.create(name="Santiago", slug="slug")
@@ -29,11 +31,18 @@ class SendNotifications(TestCase):
         self.bookCopies[book.id] = book
 
     def test_usrs_out_of_term(self):
-        bookCopiesRet = get_borrows_out_of_time(3)
+        bookCopiesRet = get_borrows_out_of_time(settings.CRON_EMAIL_NOTIFICATION_SETTINGS["BORROW_MAX_TERM_MONTH"])
 
         for bookCopy in bookCopiesRet:
             self.assertEqual(bookCopy, self.bookCopies[bookCopy.id])
 
 
     def test_send_notification_to_be_true(self):
-        self.assertTrue(send_notification(None))
+        bookCopiesList = list(self.bookCopies.values())
+        
+        send_notification(bookCopiesList)
+
+        self.assertEqual(len(mail.outbox), len(bookCopiesList))
+        self.assertEqual(mail.outbox[0].subject, settings.CRON_EMAIL_NOTIFICATION_SETTINGS["TEMPLATE_SUBJECT"])
+
+    #TODO Test for email send fail. What must the system do?
