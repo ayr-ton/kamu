37c63cc6287b5693089a4de908b2f046b9bbde82
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2017-09-26 15:33:22 -0300

    [temporary] added huge page_size for pagination for the system to keep working as it was before.

--- kamu/common_settings.py
+++ kamu/common_settings.py
@@ -88,7 +88,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_PAGINATION_CLASS': ('rest_framework.pagination.PageNumberPagination'),
-    'PAGE_SIZE': 20
+    'PAGE_SIZE': 999999
 }
 
 if os.environ.get("OKTA_METADATA_URL") != None:
