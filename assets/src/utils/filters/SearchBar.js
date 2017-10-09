db9519aab0af8db4ca6ac9585799330590155b7b
Lucas Monteiro <lucasmonteiro001@gmail.com>
2017-10-09 14:13:47 -0300

    [NOT READY FOR PULL REQUEST] extracted SearchBar class to a component in a different file

---
+++ assets/src/utils/filters/SearchBar.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import Search from 'material-ui/svg-icons/action/search';
+import Close from 'material-ui/svg-icons/navigation/close';
+import TextField from 'material-ui/TextField';
+import Paper from 'material-ui/Paper';
+
+export default class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            searchTerm: ""
+        };
+
+        this._onChange = this._onChange.bind(this);
+        this._onClear = this._onClear.bind(this);
+    }
+
+    _onChange(event, searchTerm) {
+        this.setState({searchTerm});
+        this.props.onChange(searchTerm);
+    }
+
+    _onClear() {
+        this.setState({searchTerm: ""});
+        this.props.onChange("");
+    }
+
+    render() {
+        return (
+            <Paper zDepth={3} style={{
+                position: 'fixed',
+                zIndex: 3,
+                width: '100%',
+                height: 50,
+                top: 64,
+                backgroundColor: 'white'
+            }}>
+                <div style={{position: 'relative', width: '80%', height: 50, left: 80}}>
+                    <div style={{display: 'inline-block', verticalAlign: 'middle', marginLeft: 5}}>
+                        <Search style={{color: 'gray', marginLeft: 5}}/>
+                    </div>
+                    <TextField value={this.state.searchTerm} hintText="Search"
+                               onChange={this._onChange}
+                               underlineShow={false}
+                               style={{
+                                   marginLeft: 10,
+                                   width: '90%',
+                                   height: '96%',
+                                   fontSize: 'larger',
+                                   border: 'none',
+                                   outline: 'none'
+                               }}/>
+                    {this.state.searchTerm !== "" &&
+                    <div style={{display: 'inline-block', verticalAlign: 'middle'}}>
+                        <Close style={{color: 'gray', cursor: 'pointer'}} onClick={this._onClear}/>
+                    </div>}
+                </div>
+            </Paper>
+        )
+    }
+}
