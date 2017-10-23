b7cb9af4d21cbf76da28b7d7f3968b0c2996d7de
Lucas Monteiro <lucasmonteiro001@gmail.com>
2017-10-23 14:53:59 -0200

    added test to clear search term when click in NavigationClose icon

--- assets/src/utils/filters/SearchBar.test.js
+++ assets/src/utils/filters/SearchBar.test.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import SearchBar from './SearchBar'
-import { shallow } from 'enzyme';
-import { expect } from 'chai';
-import sinon from 'sinon';
+import {shallow} from 'enzyme';
+import {expect} from 'chai';
 
 describe('<Search />', () => {
 
     let searchBarComponent;
 
     it('should render with empty search term', () => {
-        searchBarComponent = shallow(<SearchBar onChange={() => {}}/>)
+        searchBarComponent = shallow(<SearchBar onChange={() => {
+        }}/>)
         expect(searchBarComponent.state().searchTerm).to.equal("");
-    })
+    });
+
+    it('should clear search term when click in NavigationClose icon', () => {
+
+        const searchTerm = 'texto de teste';
+        const onChange = () => {
+        };
+
+        searchBarComponent = shallow(<SearchBar onChange={onChange}/>);
+
+        searchBarComponent.setState({searchTerm});
+
+        expect(searchBarComponent.state().searchTerm).to.be.equal(searchTerm);
+
+        const clear = searchBarComponent.find('NavigationClose');
+
+        clear.simulate('click');
+
+        expect(searchBarComponent.state().searchTerm).to.be.equal("");
+
+    });
 });
