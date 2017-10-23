a863b8b93853d427404b5b3a3b5a169bbd02f0b7
Lucas Monteiro <lucasmonteiro001@gmail.com>
2017-10-23 15:23:14 -0200

    added test to onChange search term

--- assets/src/utils/filters/SearchBar.test.js
+++ assets/src/utils/filters/SearchBar.test.js
@@ -2,22 +2,21 @@ import React from 'react';
 import SearchBar from './SearchBar'
 import {shallow} from 'enzyme';
 import {expect} from 'chai';
+import sinon from 'sinon';
 
 describe('<Search />', () => {
 
     let searchBarComponent;
+    const searchTerm = 'texto de teste';
+    const onChange = () => {
+    };
 
     it('should render with empty search term', () => {
-        searchBarComponent = shallow(<SearchBar onChange={() => {
-        }}/>)
+        searchBarComponent = shallow(<SearchBar onChange={onChange}/>);
         expect(searchBarComponent.state().searchTerm).to.equal("");
     });
 
     it('should clear search term when click in NavigationClose icon', () => {
-
-        const searchTerm = 'texto de teste';
-        const onChange = () => {
-        };
 
         searchBarComponent = shallow(<SearchBar onChange={onChange}/>);
 
@@ -32,4 +31,19 @@ describe('<Search />', () => {
         expect(searchBarComponent.state().searchTerm).to.be.equal("");
 
     });
+
+    it('should call onchange function passing the search term', () => {
+
+        const onChangeSpy = sinon.spy(onChange);
+
+        searchBarComponent = shallow(<SearchBar onChange={onChangeSpy}/>);
+
+        const textField = searchBarComponent.find('TextField');
+
+        textField.props().onChange({}, searchTerm);
+
+        expect(onChangeSpy.calledWith(searchTerm)).to.be.true;
+
+        expect(searchBarComponent.state().searchTerm).to.be.equal(searchTerm);
+    })
 });
