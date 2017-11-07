f6757d23c8479513403cf652112fa2fcd1889235
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2017-11-07 16:08:23 +0100

    remove template folder

--- kamu/common_settings.py
+++ kamu/common_settings.py
@@ -35,7 +35,7 @@ ROOT_URLCONF = 'kamu.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ os.path.join(BASE_DIR, "templates")],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
