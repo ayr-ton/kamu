7736f9072b4bcaf307c0b3955cd6eaaa734bb3a4
Ayrton Araujo <aaraujo@thoughtworks.com>
2018-01-14 01:16:02 -0200

    Adds autocomplete for library name

--- books/admin.py
+++ books/admin.py
@@ -15,6 +15,7 @@ class BookCopyInline(admin.TabularInline):
 class LibraryAdmin(admin.ModelAdmin):
     inlines = [BookCopyInline]
     list_display = ['name']
+    search_fields = ['name']
 
 
 class BookAdmin(admin.ModelAdmin):
@@ -27,8 +28,8 @@ class BookAdmin(admin.ModelAdmin):
 class BookCopyAdmin(admin.ModelAdmin):
     list_display = ['id', 'book', 'library', 'user']
     list_per_page = 20
-    search_fields = ['book__title', ]
-    autocomplete_fields = ['book', 'user']
+    search_fields = ['book__title', 'user__username']
+    autocomplete_fields = ['book', 'library', 'user']
 
     def add_view(self, request):
         self.exclude = ['user', 'borrow_date']
