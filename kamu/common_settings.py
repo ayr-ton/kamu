482cd5de7e0916d9c59ae8d2517589c938f06772
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2018-10-12 20:03:16 -0300

    Revert "Removed reference to stats file."

--- kamu/common_settings.py
+++ kamu/common_settings.py
@@ -76,6 +76,7 @@ STATICFILES_DIRS = [
 WEBPACK_LOADER = {
     'DEFAULT': {
         'BUNDLE_DIR_NAME': 'bundles/',
+        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
         'IGNORE': []
     }
 }
