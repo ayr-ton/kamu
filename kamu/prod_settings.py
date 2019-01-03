d9234b8c65377cd15627f2e80d68f9c1aecbd74f
Ayrton Araujo <ayr-ton@users.noreply.github.com>
2019-01-03 14:56:25 -0200

    Fetch database configuration from dokku/heroku automatically

--- kamu/prod_settings.py
+++ kamu/prod_settings.py
@@ -1,5 +1,7 @@
 from .common_settings import *
 import os
+import dj_database_url
+
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
@@ -13,13 +15,18 @@ ALLOWED_HOSTS = [os.environ['ALLOWED_HOSTS']]
 SECRET_KEY = os.environ['SECRET_KEY']
 SECURE_SSL_REDIRECT = True
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['DATABASE_NAME'],
-        'USER': os.environ['DATABASE_USER'],
-        'PASSWORD': os.environ['DATABASE_PASSWORD'],
-        'HOST': os.environ['DATABASE_HOST'],
-        'PORT': '5432',
+if os.environ['DATABASE_URL']:
+    DATABASES = {
+        'default': dj_database_url.config()
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ['DATABASE_NAME'],
+            'USER': os.environ['DATABASE_USER'],
+            'PASSWORD': os.environ['DATABASE_PASSWORD'],
+            'HOST': os.environ['DATABASE_HOST'],
+            'PORT': '5432',
+        }
+    }
