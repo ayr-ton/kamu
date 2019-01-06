a9dc5229e264e32918b5bb98c672cf137e974875
Ayrton Araújo <ayr-ton@users.noreply.github.com>
2019-01-06 19:04:49 -0200

    Revert "Add wishlist functionality"

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,4 @@
-from django import forms
-from django.contrib import admin, messages
+from django.contrib import admin
 from django.urls import path
 
 from books import views
@@ -20,16 +19,8 @@ class LibraryAdmin(admin.ModelAdmin):
     search_fields = ['name']
 
 
-class WishListInline(admin.TabularInline):
-    model = WishList
-    extra = 0
-    max_num = 0
-    can_delete = False
-    readonly_fields = ['book', 'user', 'library', 'state']
-
-
 class BookAdmin(admin.ModelAdmin):
-    inlines = [BookCopyInline, WishListInline]
+    inlines = [BookCopyInline]
     list_display = ['isbn', 'title', 'author']
     list_per_page = 20
     search_fields = ['title', 'author', 'isbn']
@@ -40,11 +31,10 @@ class BookAdmin(admin.ModelAdmin):
         info = self.model._meta.app_label, self.model._meta.model_name
 
         my_urls = [
-            path('isbn/', views.IsbnFormView.as_view(), name='%s_%s_isbn' % info),
+            path('isbn/', views.IsbnFormView.as_view(),  name='%s_%s_isbn' % info),
         ]
 
         return my_urls + urls
-
 
 class BookCopyAdmin(admin.ModelAdmin):
     list_display = ['id', 'book', 'library', 'user']
@@ -56,17 +46,9 @@ class BookCopyAdmin(admin.ModelAdmin):
         return super(BookCopyAdmin, self).add_view(request)
 
 
-class WishListAdmin(admin.ModelAdmin):
-    list_display = ['book', 'user', 'library', 'state']
-    autocomplete_fields = ['book', 'library', 'user']
-    search_fields = ['book__title', 'user__username', 'state']
-    list_per_page = 20
-
-
 admin.site.site_header = 'Kamu administration'
 admin.site.site_title = 'Kamu administration'
 admin.site.index_title = 'Kamu'
 admin.site.register(Book, BookAdmin)
 admin.site.register(Library, LibraryAdmin)
 admin.site.register(BookCopy, BookCopyAdmin)
-admin.site.register(WishList, WishListAdmin)
