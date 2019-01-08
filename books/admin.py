766a0b93ccc0d74c242597549e83a3459430e114
ederson badeca <edersonbadeca@gmail.com>
2019-01-08 13:49:44 -0200

    Import admin pages from 04487de

--- books/admin.py
+++ books/admin.py
@@ -19,8 +19,16 @@ class LibraryAdmin(admin.ModelAdmin):
     search_fields = ['name']
 
 
+class WishListInline(admin.TabularInline):
+    model = WishList
+    extra = 0
+    max_num = 0
+    can_delete = False
+    readonly_fields = ['book', 'user', 'library', 'state']
+
+
 class BookAdmin(admin.ModelAdmin):
-    inlines = [BookCopyInline]
+    inlines = [BookCopyInline, WishListInline]
     list_display = ['isbn', 'title', 'author']
     list_per_page = 20
     search_fields = ['title', 'author', 'isbn']
@@ -31,10 +39,11 @@ class BookAdmin(admin.ModelAdmin):
         info = self.model._meta.app_label, self.model._meta.model_name
 
         my_urls = [
-            path('isbn/', views.IsbnFormView.as_view(),  name='%s_%s_isbn' % info),
+            path('isbn/', views.IsbnFormView.as_view(), name='%s_%s_isbn' % info),
         ]
 
         return my_urls + urls
+
 
 class BookCopyAdmin(admin.ModelAdmin):
     list_display = ['id', 'book', 'library', 'user']
@@ -46,9 +55,19 @@ class BookCopyAdmin(admin.ModelAdmin):
         return super(BookCopyAdmin, self).add_view(request)
 
 
+
+
+class WishListAdmin(admin.ModelAdmin):
+    list_display = ['book', 'user', 'library', 'state']
+    autocomplete_fields = ['book', 'library', 'user']
+    search_fields = ['book__title', 'user__username', 'state']
+    list_per_page = 20
+
+
 admin.site.site_header = 'Kamu administration'
 admin.site.site_title = 'Kamu administration'
 admin.site.index_title = 'Kamu'
 admin.site.register(Book, BookAdmin)
 admin.site.register(Library, LibraryAdmin)
 admin.site.register(BookCopy, BookCopyAdmin)
+admin.site.register(WishList, WishListAdmin)
