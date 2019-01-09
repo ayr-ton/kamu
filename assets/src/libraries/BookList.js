1df794a80347338ae19dbcc6f04c36648e867a09
Rita Moraes <ritac.moraes@gmail.com>
2019-01-09 19:36:20 -0200

    Improvment of the Add wishlist button style

--- assets/src/libraries/BookList.js
+++ assets/src/libraries/BookList.js
@@ -1,127 +1,140 @@
-import React, {Component} from 'react';
-import Book from './Book';
-import ProfileService from '../services/ProfileService';
-import InfiniteScroll from 'react-infinite-scroller';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import debounce from 'lodash/debounce'
-import SearchBar from '../utils/filters/SearchBar';
+import React, { Component } from "react";
+import Book from "./Book";
+import ProfileService from "../services/ProfileService";
+import InfiniteScroll from "react-infinite-scroller";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import debounce from "lodash/debounce";
+import SearchBar from "../utils/filters/SearchBar";
 import Button from "@material-ui/core/Button";
 
 export default class BookList extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            books: [],
-            currentBook: {},
-            hasMoreItems: true,
-            isLoading: false,
-            nextPage: 1,
-            searchTerm: ""
+    this.state = {
+      books: [],
+      currentBook: {},
+      hasMoreItems: true,
+      isLoading: false,
+      nextPage: 1,
+      searchTerm: ""
+    };
+
+    this._loadBooks = this._loadBooks.bind(this);
+    this._handleSearch = this._handleSearch.bind(this);
+    this._loadMoreBooks = this._loadMoreBooks.bind(this);
+    this._onLoadMoreBooksComplete = this._onLoadMoreBooksComplete.bind(this);
+    this._onLoadWithSearchTerm = this._onLoadWithSearchTerm.bind(this);
+    this._renderBookItem = this._renderBookItem.bind(this);
+  }
+
+  _loadBooks(page, callback, searchTerm = "") {
+    const { isLoading } = this.state;
+    const { service, librarySlug } = this.props;
+
+    if (!isLoading) {
+      this.setState({ isLoading: true, hasMoreItems: false });
+
+      return service
+        .getBooksByPage(librarySlug, page, searchTerm)
+        .then(callback);
+    }
+  }
+
+  _handleSearch(searchTerm) {
+    this.setState({ searchTerm });
+
+    window.scroll(0, 0);
+
+    this._loadBooks(1, this._onLoadWithSearchTerm, searchTerm);
+  }
+
+  _onLoadWithSearchTerm(response) {
+    this.setState({
+      hasMoreItems: !!response.next,
+      books: response.results,
+      isLoading: false,
+      nextPage: 2
+    });
+  }
+
+  _loadMoreBooks() {
+    const { nextPage, searchTerm } = this.state;
+
+    this._loadBooks(nextPage, this._onLoadMoreBooksComplete, searchTerm);
+  }
+
+  _onLoadMoreBooksComplete(response) {
+    if (!response) {
+      this.setState({
+        hasMoreItems: false,
+        isLoading: false
+      });
+    } else {
+      this.setState(previous => {
+        return {
+          hasMoreItems: !!response.next,
+          books: previous.books.concat(response.results),
+          isLoading: false,
+          nextPage: ++previous.nextPage
         };
-
-        this._loadBooks = this._loadBooks.bind(this);
-        this._handleSearch = this._handleSearch.bind(this);
-        this._loadMoreBooks = this._loadMoreBooks.bind(this);
-        this._onLoadMoreBooksComplete = this._onLoadMoreBooksComplete.bind(this);
-        this._onLoadWithSearchTerm = this._onLoadWithSearchTerm.bind(this);
-        this._renderBookItem = this._renderBookItem.bind(this);
+      });
     }
+  }
 
-    _loadBooks(page, callback, searchTerm = "") {
-        const {isLoading} = this.state;
-        const {service, librarySlug} = this.props;
+  _redirectToAddWishlist() {
+    window.location.href = "/admin/books/wishlist/add/";
+  }
 
-        if (!isLoading) {
-            this.setState({isLoading: true, hasMoreItems: false});
-
-            return service.getBooksByPage(librarySlug, page, searchTerm).then(callback);
-        }
-    }
-
-    _handleSearch(searchTerm) {
-
-        this.setState({searchTerm});
-
-        window.scroll(0, 0);
-
-        this._loadBooks(1, this._onLoadWithSearchTerm, searchTerm);
-    }
-
-    _onLoadWithSearchTerm(response) {
-        this.setState({
-            hasMoreItems: !!response.next,
-            books: response.results,
-            isLoading: false,
-            nextPage: 2
-        })
-    }
-
-    _loadMoreBooks() {
-        const {nextPage, searchTerm} = this.state;
-
-        this._loadBooks(nextPage, this._onLoadMoreBooksComplete, searchTerm);
-
-    }
-
-    _onLoadMoreBooksComplete(response) {
-
-        if (!response) {
-            this.setState({
-                hasMoreItems: false,
-                isLoading: false,
-            });
-        } else {
-            this.setState((previous) => {
-                return {
-                    hasMoreItems: !!response.next,
-                    books: previous.books.concat(response.results),
-                    isLoading: false,
-                    nextPage: ++previous.nextPage
-                };
-            });
-        }
-    }
-
-    _redirectToAddWishlist(){
-        window.location.href = "/admin/books/wishlist/add/"
-    }
-
-    render() {
-        let content;
-        const profileService = new ProfileService();
-        const library = profileService.getRegion();
-        const loader = (
-            <div style={{padding: 10, textAlign: "center"}} key='booklist-loader'>
-                <CircularProgress/>
-            </div>);
-        if (this.state.books) {
-            if (this.state.books.length == 0 && !this.state.isLoading) {
-                content = <Button className="btn-borrow" onClick={this._redirectToAddWishlist}>Add book to Wishlist</Button>;
-            } else {
-                content = this.state.books.map(book => this._renderBookItem(book, library));
-            }
-        }
-
-
-        return (
-            <div>
-                <SearchBar onChange={debounce(this._handleSearch, 300)}/>
-                <InfiniteScroll
-                    pageStart={0}
-                    loadMore={this._loadMoreBooks}
-                    hasMore={this.state.hasMoreItems}
-                    threshold={950}
-                    loader={loader}>
-                    <div className="book-list">
-                        {content}
-                    </div>
-                </InfiniteScroll>
-            </div>
+  render() {
+    let content;
+    const profileService = new ProfileService();
+    const library = profileService.getRegion();
+    const loader = (
+      <div style={{ padding: 10, textAlign: "center" }} key="booklist-loader">
+        <CircularProgress />
+      </div>
+    );
+    if (this.state.books) {
+      if (this.state.books.length == 0 && !this.state.isLoading) {
+        content = (
+          <div className="wishlist">
+            <Button onClick={this._redirectToAddWishlist}>
+              Add book to Wishlist
+            </Button>
+          </div>
         );
+      } else {
+        content = this.state.books.map(book =>
+          this._renderBookItem(book, library)
+        );
+      }
     }
 
-    _renderBookItem(book, library) {
-        return <Book key={book.id} book={book} service={this.props.service} library={library}/>
-    }
+    return (
+      <div>
+        <SearchBar onChange={debounce(this._handleSearch, 300)} />
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={this._loadMoreBooks}
+          hasMore={this.state.hasMoreItems}
+          threshold={950}
+          loader={loader}
+        >
+          <div className="book-list">{content}</div>
+        </InfiniteScroll>
+      </div>
+    );
+  }
+
+  _renderBookItem(book, library) {
+    return (
+      <Book
+        key={book.id}
+        book={book}
+        service={this.props.service}
+        library={library}
+      />
+    );
+  }
 }
