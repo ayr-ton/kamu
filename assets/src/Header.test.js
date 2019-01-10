5d0f6536b252ee26f00c4459394f19e3fb496734
Rita Moraes <ritac.moraes@gmail.com>
2019-01-10 11:25:04 -0200

    Fixes redirection test

--- assets/src/Header.test.js
+++ assets/src/Header.test.js
@@ -1,52 +1,51 @@
 import React from "react";
 import Header from "./Header";
-import {shallow} from "enzyme";
-import {expect} from "chai";
+import { shallow } from "enzyme";
+import { expect } from "chai";
 import sinon from "sinon";
 
-describe('Header', () => {
-    let profileService;
-    let header;
+describe("Header", () => {
+  let profileService;
+  let header;
 
-    beforeEach(() => {
-        global.window = {location: {href: ''}};
+  beforeEach(() => {
+    global.window = { location: { href: "" } };
 
-        profileService = {
-            getLoggedUser: () => Promise.resolve({}),
-            clearRegion: () => {
-            }
-        };
-    });
+    profileService = {
+      getLoggedUser: () => Promise.resolve({}),
+      clearRegion: () => {}
+    };
+  });
 
-    function renderHeader() {
-        header = shallow(<Header service={profileService}/>);
-    }
+  function renderHeader() {
+    header = shallow(<Header service={profileService} />);
+  }
 
-    it('should clear the region and redirect to home', () => {
-        const clearRegion = sinon.spy(profileService, 'clearRegion');
+  it("should clear the region and redirect to home", () => {
+    const clearRegion = sinon.spy(profileService, "clearRegion");
 
-        renderHeader();
-        header.instance()._changeRegion();
+    renderHeader();
+    header.instance()._changeRegion();
 
-        expect(clearRegion.called).to.be.true;
-        expect(global.window.location.href).to.equal('/');
-    });
+    expect(clearRegion.called).to.be.true;
+    expect(global.window.location.href).to.equal("/");
+  });
 
-    it('should redirect to admin wishlist', () => {
-        renderHeader();
-        header.instance()._showWishList();
-        expect(global.window.location.href).to.equal('/admin/books/wishlist');
-    });
+  it("should redirect to admin wishlist", () => {
+    renderHeader();
+    header.instance()._showWishList();
+    expect(global.window.location.href).to.equal("/admin/books/wishlist/");
+  });
 
-    it('should display the change library menu', () => {
-        global.window.location.pathname = '/foo';
-        renderHeader();
-        expect(header.state('displaysMenu')).to.be.true;
-    });
+  it("should display the change library menu", () => {
+    global.window.location.pathname = "/foo";
+    renderHeader();
+    expect(header.state("displaysMenu")).to.be.true;
+  });
 
-    it('should not display the change library menu in home', () => {
-        global.window.location.pathname = '/';
-        renderHeader();
-        expect(header.state('displaysMenu')).to.be.false;
-    });
+  it("should not display the change library menu in home", () => {
+    global.window.location.pathname = "/";
+    renderHeader();
+    expect(header.state("displaysMenu")).to.be.false;
+  });
 });
