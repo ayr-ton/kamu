9e45ba114a60c395e072b608aa82805bf028baeb
Mario Cecchi <mcecchi@thoughtworks.com>
2019-02-16 19:01:14 -0200

    Add route for my books

--- assets/src/Main.js
+++ assets/src/Main.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './Header';
 import LibrarySelector from './home/LibrarySelector';
 import Library from './libraries/Library';
+import MyBooks from './mybooks/MyBooks';
 
 function Main() {
   return (
@@ -11,6 +12,7 @@ function Main() {
         <Header />
         <div id="content">
           <Route exact path="/" component={LibrarySelector} />
+          <Route exact path="/my-books" component={MyBooks} />
           <Route path="/libraries/:slug" render={({ match }) => (
             <Library slug={match.params.slug} />
           )} />
