6c89d7b9e53f644603f5768e3bfee2419c1cb5a0
Mario Cecchi <mcecchi@thoughtworks.com>
2019-02-19 21:15:10 -0300

    Add library redirector to initial page

--- assets/src/App.js
+++ assets/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './Header';
 import LibrarySelector from './home/LibrarySelector';
+import LibraryRedirector from './home/LibraryRedirector';
 import Library from './libraries/Library';
 import MyBooks from './mybooks/MyBooks';
 import { getLoggedUser } from './services/ProfileService';
@@ -29,10 +30,14 @@ class App extends Component {
           <Header borrowedBooksCount={this.state.user ? this.state.user.borrowed_books_count : 0} />
             {this.state.user && (
               <div id="content">
-              <Route exact path="/" component={LibrarySelector} />
-              <Route exact path="/my-books" component={MyBooks} />
-              <Route path="/libraries/:slug" render={({ match }) => (
-                <Library slug={match.params.slug} />
+                <Route exact path="/" render={() => (
+                  <LibraryRedirector>
+                    <LibrarySelector />
+                  </LibraryRedirector>
+                )} />
+                <Route exact path="/my-books" component={MyBooks} />
+                <Route path="/libraries/:slug" render={({ match }) => (
+                  <Library slug={match.params.slug} />
                 )} />
               </div>
             )}
