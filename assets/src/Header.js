5e0bbc4c71dd631f3892ce699bff6893f92d2689
Mario Cecchi <mcecchi@thoughtworks.com>
2019-02-19 21:01:37 -0300

    Make Header a function component instead of class

--- assets/src/Header.js
+++ assets/src/Header.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import AppBar from "@material-ui/core/AppBar";
 import IconButton from "@material-ui/core/IconButton";
@@ -10,61 +10,56 @@ import { HOME_URL, ADMIN_URL, MY_BOOKS_URL, ADD_BOOK_URL, LIBRARY_URL_PREFIX } f
 const redirectExternal = (url) => window.location.assign(url);
 const getHomeEndpoint = () => getRegion() ? `${LIBRARY_URL_PREFIX}/${getRegion()}` : HOME_URL;
 
-class Header extends Component {
-	redirect(url) {
-		this.props.history.push(url);
-	}
+function Header({ showMenu, borrowedBooksCount, history }) {
+  const redirect = (url) => history.push(url);
+  return (
+    <AppBar	className="header">
+      <Toolbar>
+        <a href={HOME_URL} className="header-content">
+          <img src="/static/images/logo.svg" alt="Kamu logo" />
+        </a>
 
-	render() {
-		return (
-				<AppBar	className="header">
-					<Toolbar>
-						<a href={HOME_URL} className="header-content">
-							<img src="/static/images/logo.svg" alt="Kamu logo" />
-						</a>
+        <div style={{flexGrow: 1}} />
 
-						<div style={{flexGrow: 1}} />
+        {showMenu && (
+          <div className="header-menu">
+            <IconButton title="Library home" id="home-button" onClick={() => redirect(getHomeEndpoint()) }>
+              <Icon className="fa fa-home" />
+            </IconButton>
 
-						{this.props.showMenu && (
-							<div className="header-menu">
-								<IconButton title="Library home" id="home-button" onClick={() => this.redirect(getHomeEndpoint()) }>
-									<Icon className="fa fa-home" />
-								</IconButton>
+            <IconButton title="My books" id="my-books-button" onClick={() => redirect(MY_BOOKS_URL) }>
+              <Badge badgeContent={borrowedBooksCount} color="secondary">
+                <Icon className="fa fa-book-reader" />
+              </Badge>
+            </IconButton>
 
-								<IconButton title="My books" id="my-books-button" onClick={() => this.redirect(MY_BOOKS_URL) }>
-									<Badge badgeContent={this.props.borrowedBooksCount} color="secondary">
-										<Icon className="fa fa-book-reader" />
-									</Badge>
-								</IconButton>
+            <IconButton title="Add a book" id="add-book-button" onClick={() => redirectExternal(ADD_BOOK_URL)}>
+              <Icon className="fa fa-plus-circle" />
+            </IconButton>
 
-								<IconButton title="Add a book" id="add-book-button" onClick={() => redirectExternal(ADD_BOOK_URL)}>
-									<Icon className="fa fa-plus-circle" />
-								</IconButton>
+            <IconButton title="Change region" id="change-region-button" onClick={() => { clearRegion(); redirect(HOME_URL); }}>
+              <Icon className="fa fa-map-marker-alt" />
+            </IconButton>
 
-								<IconButton title="Change region" id="change-region-button" onClick={() => { clearRegion(); this.redirect(HOME_URL); }}>
-									<Icon className="fa fa-map-marker-alt" />
-								</IconButton>
-
-								<IconButton title="Administration" id="admin-button" onClick={() => redirectExternal(ADMIN_URL)}>
-									<Icon className="fa fa-cog" />
-								</IconButton>
-							</div>
-						)}
-					</Toolbar>
-				</AppBar>
-		);
-	}
+            <IconButton title="Administration" id="admin-button" onClick={() => redirectExternal(ADMIN_URL)}>
+              <Icon className="fa fa-cog" />
+            </IconButton>
+          </div>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 Header.propTypes = {
-	showMenu: PropTypes.bool,
-	borrowedBooksCount: PropTypes.number,
-	history: PropTypes.shape({}).isRequired,
+  showMenu: PropTypes.bool,
+  borrowedBooksCount: PropTypes.number,
+  history: PropTypes.shape({}).isRequired,
 };
 
 Header.defaultProps = {
-	showMenu: true,
-	borrowedBooksCount: 0,
+  showMenu: true,
+  borrowedBooksCount: 0,
 }
 
 export { Header };
