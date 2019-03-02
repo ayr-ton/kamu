75ae9c52ee62b684f332cee6f7d3894f7df66235
Mario Cecchi <mcecchi@thoughtworks.com>
2019-03-02 13:36:51 -0300

    Set Analytics account via environment variable and disable it entirely if it's not set

--- kamu/urls.py
+++ kamu/urls.py
@@ -42,5 +42,5 @@ urlpatterns = login_routes + [
     url(r'^api/copies/(?P<id>.+)/borrow', views.BookCopyBorrowView.as_view()),
     url(r'^api/copies/(?P<id>.+)/return', views.BookCopyReturnView.as_view()),
     url(r'^favicon\.ico$', RedirectView.as_view(url=settings.STATIC_URL + 'images/favicon.ico')),
-    url(r'', login_required(TemplateView.as_view(template_name='index.html'))),
+    url(r'', login_required(views.FrontendView.as_view())),
 ]
