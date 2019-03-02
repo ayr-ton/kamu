a22d27a1e462e000b251f4a2769a58398610c38a
Lucas Hanke <lhanke@thoughtworks.com>
2019-03-02 12:15:15 -0300

    makes logo anchor take advantage of react routerb while still being accessible

--- assets/src/Header.jsx
+++ assets/src/Header.jsx
@@ -19,7 +19,7 @@ function Header({ borrowedBooksCount, history }) {
   return (
     <AppBar className="header">
       <Toolbar>
-        <a href={getHomeEndpoint()} className="header-content">
+        <a href={getHomeEndpoint()} className="header-content" onClick={(e) => { e.preventDefault(); redirect(getHomeEndpoint()); }}>
           <img src="/static/images/logo.svg" alt="Kamu logo" />
         </a>
 
