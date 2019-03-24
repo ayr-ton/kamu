5f90f16cc398509a42687f425b86a7c2ae905cc0
Mario Cecchi <mcecchi@thoughtworks.com>
2019-03-24 20:47:37 -0300

    Render app content without waiting for user profile

--- assets/src/components/App.jsx
+++ assets/src/components/App.jsx
@@ -28,26 +28,24 @@ class App extends Component {
       <BrowserRouter>
         <React.Fragment>
           <Header borrowedBooksCount={user ? user.borrowed_books_count : 0} />
-          {user && (
-            <div id="content">
-              <Route
-                exact
-                path="/"
-                render={({ history }) => (
-                  <LibraryRedirector history={history}>
-                    <LibrarySelector />
-                  </LibraryRedirector>
-                )}
-              />
-              <Route exact path="/my-books" component={MyBooks} />
-              <Route
-                path="/libraries/:slug"
-                render={({ match, history }) => (
-                  <Library slug={match.params.slug} history={history} />
-                )}
-              />
-            </div>
-          )}
+          <div id="content">
+            <Route
+              exact
+              path="/"
+              render={({ history }) => (
+                <LibraryRedirector history={history}>
+                  <LibrarySelector />
+                </LibraryRedirector>
+              )}
+            />
+            <Route exact path="/my-books" component={MyBooks} />
+            <Route
+              path="/libraries/:slug"
+              render={({ match, history }) => (
+                <Library slug={match.params.slug} history={history} />
+              )}
+            />
+          </div>
           <Route path="/" render={trackAnalyticsPageView} />
         </React.Fragment>
       </BrowserRouter>
