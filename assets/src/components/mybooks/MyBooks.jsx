df7be31b46db57fec7fc995fa6a4f0011fb31c51
Lucas Hanke <lhanke@thoughtworks.com>
2019-06-07 18:11:48 -0300

    change MyBooks to use hooks instead of internal state

--- assets/src/components/mybooks/MyBooks.jsx
+++ assets/src/components/mybooks/MyBooks.jsx
@@ -1,31 +1,29 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Paper from '@material-ui/core/Paper';
 import Icon from '@material-ui/core/Icon';
 import { getMyBooks } from '../../services/BookService';
 import BookList from '../books/BookList';
 
-export default class MyBooks extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      books: [],
-    };
-  }
+const MyBooks = () => {
+  const [books, setBooks] = useState([]);
 
-  async componentDidMount() {
-    const response = await getMyBooks();
-    this.setState({ books: response.results });
-  }
+  useEffect(() => {
+    async function fetchBooks() {
+      setBooks((await getMyBooks()).results);
+    }
 
-  render() {
-    return (
-      <React.Fragment>
-        <Paper elevation={10} className="page-title">
-          <Icon className="fa fa-book-reader" />
-          My books
-        </Paper>
-        <BookList books={this.state.books} />
-      </React.Fragment>
-    );
-  }
-}
+    fetchBooks();
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Paper elevation={10} className="page-title">
+        <Icon className="fa fa-book-reader" />
+        My books
+      </Paper>
+      <BookList books={books} />
+    </React.Fragment>
+  );
+};
+
+export default MyBooks;
