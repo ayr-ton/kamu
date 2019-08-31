d1bfec796a1a53cca431325785ec5ad5e7f14a3a
Mario Cecchi <mcecchi@thoughtworks.com>
2019-08-31 15:31:32 -0300

    Refactor page section to a component

--- assets/src/components/mybooks/MyBooks.jsx
+++ assets/src/components/mybooks/MyBooks.jsx
@@ -1,30 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { getMyBooks, getWaitlistBooks } from '../../services/BookService';
 import BookListLoader from '../books/BookListLoader';
 import { isWaitlistFeatureActive } from '../../utils/toggles';
 
 const MyBooks = () => (
   <div className="my-books" data-testid="my-books-wrapper">
-    <section>
-      <h1 className="section-title">Borrowed with me</h1>
-
+    <PageSection title="Borrowed with me">
       <BookListLoader
         source={getMyBooks}
         noBooksMessage="To borrow a book, click &ldquo;Borrow&rdquo; on a book that is available in the library."
       />
-    </section>
+    </PageSection>
 
     {isWaitlistFeatureActive() && (
-      <section>
-        <h1 className="section-title">On my wait list</h1>
-
+      <PageSection title="On my wait list">
         <BookListLoader
           source={getWaitlistBooks}
           noBooksMessage="To add a book to your wait list, click &ldquo;Join the waitlist&rdquo; on a book that is not available."
         />
-      </section>
+      </PageSection>
     )}
   </div>
 );
+
+const PageSection = ({ title, children }) => (
+  <section>
+    <h1 className="section-title">{title}</h1>
+    {children}
+  </section>
+);
+
+PageSection.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.element.isRequired,
+};
 
 export default MyBooks;
