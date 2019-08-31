c105686795b86b5acc7c0fee29673b9e9a2cb8ed
Mario Cecchi <mcecchi@thoughtworks.com>
2019-08-31 15:35:40 -0300

    Remove unnecessary prop

--- assets/src/components/LoadingIndicator.jsx
+++ assets/src/components/LoadingIndicator.jsx
@@ -1,25 +1,16 @@
 import React from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import PropTypes from 'prop-types';
 
-function LoadingIndicator(props) {
+function LoadingIndicator() {
   return (
     <div
       style={{ padding: 10, textAlign: 'center' }}
       key="booklist-loader"
-      data-testid={props['data-testid']}
+      data-testid="loading-indicator"
     >
       <CircularProgress />
     </div>
   );
 }
-
-LoadingIndicator.propTypes = {
-  'data-testid': PropTypes.string,
-};
-
-LoadingIndicator.defaultProps = {
-  'data-testid': null,
-};
 
 export default LoadingIndicator;
