ec53d15ea180b2d1c205df181d749d39be0d5b32
Mario Cecchi <mcecchi@thoughtworks.com>
2019-08-31 15:25:02 -0300

    Fix python styling issues

--- waitlist/admin.py
+++ waitlist/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from waitlist.models import WaitlistItem
 
+
 class WaitlistItemAdmin(admin.ModelAdmin):
     model = WaitlistItem
     list_display = ['id', 'book', 'library', 'user']
     search_fields = ['book__title', 'user__username']
     autocomplete_fields = ['book', 'library', 'user']
+
 
 admin.site.register(WaitlistItem, WaitlistItemAdmin)
