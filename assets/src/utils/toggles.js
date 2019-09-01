e8a13ffcd61d5aeabda8f5a2c317b785b3c347b9
Mario Cecchi <mcecchi@thoughtworks.com>
2019-09-01 12:45:09 -0300

    Add default export to toggles helper

--- assets/src/utils/toggles.js
+++ assets/src/utils/toggles.js
@@ -1,7 +1,10 @@
 import parse from 'url-parse';
 
-// eslint-disable-next-line import/prefer-default-export
 export const isWaitlistFeatureActive = () => {
   const { query } = parse(window.location.href, true);
   return 'waitlist' in query && query.waitlist === 'active';
+};
+
+export default {
+  isWaitlistFeatureActive,
 };
