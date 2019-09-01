b586efb67dc40d1f120731e7c79c5c8f1b6ba026
Mario Cecchi <mcecchi@thoughtworks.com>
2019-09-01 14:03:03 -0300

    Add leave waitlist button in the frontend

--- assets/src/components/books/Book.test.jsx
+++ assets/src/components/books/Book.test.jsx
@@ -9,6 +9,7 @@ import {
   someBookWithNoAvailableCopies,
   someBookWithACopyFromMe,
   someBookThatCanBeAddedToWaitlist,
+  someBookThatIsInMyWaitlist,
 } from '../../../test/booksHelper';
 import { borrowBook, returnBook, joinWaitlist } from '../../services/BookService';
 
@@ -36,7 +37,15 @@ expect.extend({
     const pass = button.exists()
           && button.children().text() === 'Join the waitlist'
           && button.length === 1;
-    return { pass, message: () => 'expected component to have a waitlist button' };
+    return { pass, message: () => 'expected component to have a join waitlist button' };
+  },
+
+  toHaveLeaveWaitlistButton(received) {
+    const button = received.find(Button);
+    const pass = button.exists()
+          && button.children().text() === 'Leave the waitlist'
+          && button.length === 1;
+    return { pass, message: () => 'expected component to have a leave waitlist button' };
   },
 });
 
@@ -145,6 +154,13 @@ describe('Book', () => {
 
       expect(joinWaitlist).toHaveBeenCalledWith(book);
       expect(bookComponent).not.toHaveJoinWaitlistButton();
+    });
+
+    it('shows the leave waitlist button when book is on waitlist', async () => {
+      const book = someBookThatIsInMyWaitlist();
+      const bookComponent = createComponent(book);
+
+      expect(bookComponent).toHaveLeaveWaitlistButton();
     });
   });
 
