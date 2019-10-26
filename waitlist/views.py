7992cf498d4f0259ef04f1e273542661cab5804d
Lucas Hanke <lhanke@thoughtworks.com>
2019-10-26 18:48:52 -0300

    :sparkles: add endpoint to check the waitlist status of a particular book and user

--- waitlist/views.py
+++ waitlist/views.py
@@ -3,11 +3,12 @@ from django.shortcuts import render
 from filters.mixins import FiltersMixin
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from django.utils import timezone
 
 from books.models import Book, Library
 from books.serializers import BookSerializer
-from .models import WaitlistItem
+from .models import WaitlistItem, Waitlist
 from .serializers import WaitlistItemSerializer
 
 
@@ -33,6 +34,12 @@ class WaitlistViewSet(FiltersMixin, viewsets.ModelViewSet):
             return Response({
                 'message': str(error),
             }, status=409)
+
+    @action(detail=False, methods=['get'])
+    def check(self, request, library_slug=None, book_pk=None):
+        return Response({
+            'status': Waitlist(book_pk,library_slug).status_for(request.user),
+        }, status=200)
 
     def delete(self, request, library_slug=None, book_pk=None):
         library = Library.objects.get(slug=library_slug)
