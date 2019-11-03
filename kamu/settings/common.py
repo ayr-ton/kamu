467680ed8e4a76f9eb8b26a0b86a335c741ff06c
Ayrton Araujo <root@ayr-ton.net>
2019-11-03 12:46:26 -0300

    Add Darkly template to DRF

--- kamu/settings/common.py
+++ kamu/settings/common.py
@@ -45,7 +45,7 @@ ROOT_URLCONF = 'kamu.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'kamu/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
