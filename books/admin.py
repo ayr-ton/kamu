eaf39cab6da01281bc6f2b7080faf6e1e8c7d587
ederson badeca <edersonbadeca@gmail.com>
2019-11-15 17:31:47 -0300

    Customizes export fields

--- books/admin.py
+++ books/admin.py
@@ -6,12 +6,32 @@ from .models import *
 from import_export import resources
 from import_export.admin import ExportMixin
 from import_export.admin import ExportActionMixin
+from import_export import fields
+from import_export.widgets import ForeignKeyWidget
 
 
 class BookCopyResource(resources.ModelResource):
+    title = fields.Field(attribute='book__title', column_name='Title')
+    publisher = fields.Field(attribute='book__publisher', column_name='Publisher')
+    isbn = fields.Field(attribute='book__isbn', column_name='ISBN')
+    publication_date = fields.Field(attribute='book__publication_date', column_name='Publication date')
+    username = fields.Field(attribute='user__username', column_name='user')
+    user_email = fields.Field(attribute='user__email', column_name='Email')
+    borrow_date = fields.Field(attribute='borrow_date', column_name='Borrow date')
+    library = fields.Field(attribute='library__slug', column_name='Library')
+
     class Meta:
         model = BookCopy
-        fields = ('book__title', 'book__publisher', 'book__isbn', 'book__publication_date', 'library__slug')
+        exclude = ('id', 'book', 'user')
+        export_order = ('title',
+                        'publisher',
+                        'isbn',
+                        'publication_date',
+                        'user_email',
+                        'borrow_date',
+                        'library'
+                        )
+
 
 class BookCopyInline(admin.TabularInline):
     model = BookCopy
@@ -43,6 +63,7 @@ class BookAdmin(admin.ModelAdmin):
         ]
 
         return my_urls + urls
+
 
 class BookCopyAdmin(ExportMixin, admin.ModelAdmin):
     resource_class = BookCopyResource
