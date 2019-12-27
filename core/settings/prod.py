279ded0cf718af4308a9b42592eefbb84c8f9960
Ayrton Araujo <root@ayr-ton.net>
2019-12-27 01:17:49 -0300

    Fix collect static issue in prod like envs

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -1,10 +1,8 @@
 from .common import *
-from whitenoise import WhiteNoise
 import os
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 SECURE_SSL_REDIRECT = config('SSL', default=True, cast=bool)
 
