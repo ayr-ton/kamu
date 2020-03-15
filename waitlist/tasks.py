805fb119a76babd69acc7b5b4cdd0004ce7bcecf
Mario Cecchi <macecchi@gmail.com>
2020-03-15 19:32:07 -0300

    Create task to notify that a book on waitlist has become available

--- waitlist/tasks.py
+++ waitlist/tasks.py
@@ -21,8 +21,8 @@ def send_new_user_on_waitlist_notification(waitlist_item_id):
         logger.debug(f'Exiting task because the book {book.title} has no borrowers in {waitlist_item.library.name}')
         return
 
-    borrowers_email_list = [user.email for user in borrowers]
-    logger.info(f'Sending waitlist notification email for {borrowers_email_list}')
+    email_list = [user.email for user in borrowers]
+    logger.info(f'Sending waitlist notification email to {email_list}')
 
     waitlist_user_name = f'{waitlist_item.user.first_name} {waitlist_item.user.last_name}'
     subject = f'{waitlist_user_name} is waiting for the book {book.title} on Kamu'
@@ -36,7 +36,37 @@ def send_new_user_on_waitlist_notification(waitlist_item_id):
         subject,
         message,
         settings.EMAIL_FROM,
-        borrowers_email_list,
+        email_list,
     )
 
     logger.info(f'Waitlist email notification sent successfully')
+
+
+def send_waitlist_book_available_notification(book_copy_id):
+    book_copy = apps.get_model('books', 'BookCopy').objects.get(pk=book_copy_id)
+    book = book_copy.book
+    waitlist_items = apps.get_model('waitlist', 'WaitlistItem').objects.filter(book=book, library=book_copy.library)
+    users_on_waitlist = [item.user for item in waitlist_items]
+
+    logger.info(f'Starting a waitlist notification task for book {book.title} available' +
+                f'({len(users_on_waitlist)} users on waitlist in {book_copy.library.name})')
+
+    if len(users_on_waitlist) == 0:
+        logger.debug(f'Exiting task because the book {book.title} has no users on waitlist in {book_copy.library.name}')
+        return
+
+    email_list = [user.email for user in users_on_waitlist]
+    logger.info(f'Sending waitlist book available notification email to {email_list}')
+
+    subject = f'{book.title} is now available on Kamu'
+    message = render_to_string('waitlist_book_available_email.txt', {
+        'book_title': book.title,
+        'library_name': book_copy.library.name,
+    })
+
+    mail.send_mail(
+        subject,
+        message,
+        settings.EMAIL_FROM,
+        email_list,
+    )
