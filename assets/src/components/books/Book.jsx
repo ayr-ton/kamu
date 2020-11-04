544dbf3f371ea5d7b1792bcd3155c292f7a1021a
Mario Cecchi <mcecchi@thoughtworks.com>
2020-11-04 15:04:46 -0300

    Convert Book component to function and avoid unnecessary rerenders when hovering

--- assets/src/components/books/Book.jsx
+++ assets/src/components/books/Book.jsx
@@ -1,68 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Paper } from '@material-ui/core';
 
 import BookActionButton from './BookActionButton';
 import WaitlistIndicator from './WaitlistIndicator';
 import { BookPropType } from '../../utils/propTypes';
-
-import './Book.css';
 import { OPEN_BOOK_ACTION } from '../../utils/constants';
 
-class Book extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      zDepth: 1,
-    };
+import './Book.css';
 
-    this.onMouseOver = this.onMouseOver.bind(this);
-    this.onMouseOut = this.onMouseOut.bind(this);
-  }
+function Book({ book, onAction }) {
+  const [isHighlighted, setIsHighlighted] = React.useState(false);
+  const isOnUsersWaitlist = book.waitlist_added_date != null;
 
-  onMouseOver() { return this.setState({ zDepth: 5 }); }
+  const bookCover = {
+    backgroundImage: `url('${book.image_url}')`,
+  };
 
-  onMouseOut() { this.setState({ zDepth: 1 }); }
+  const openDetail = () => onAction(OPEN_BOOK_ACTION);
 
-  render() {
-    const { book } = this.props;
-    const isOnUsersWaitlist = book.waitlist_added_date != null;
-
-    const bookCover = {
-      backgroundImage: `url('${book.image_url}')`,
-    };
-
-    const openDetail = () => this.props.onAction(OPEN_BOOK_ACTION);
-
-    return (
-      <Paper
-        className="book"
-        data-testid="book-container"
-        elevation={this.state.zDepth}
-        onMouseOver={this.onMouseOver}
-        onFocus={this.onMouseOver}
-        onMouseOut={this.onMouseOut}
-        onBlur={this.onMouseOut}
-      >
-        <div role="button" className="book-info" onClick={openDetail} onKeyPress={openDetail} tabIndex={0}>
-          <div className="book-cover" style={bookCover} data-testid="book-cover">
-            <div className="book-cover-overlay" />
-          </div>
-
-          <div className="book-details">
-            <h1 className="book-title">{book.title}</h1>
-            <h2 className="book-author">{book.author}</h2>
-          </div>
+  return (
+    <Paper
+      className="book"
+      data-testid="book-container"
+      elevation={isHighlighted ? 5 : 1}
+      onMouseEnter={() => setIsHighlighted(true)}
+      onFocus={() => setIsHighlighted(true)}
+      onMouseLeave={() => setIsHighlighted(false)}
+      onBlur={() => setIsHighlighted(false)}
+    >
+      <div role="button" className="book-info" onClick={openDetail} onKeyPress={openDetail} tabIndex={0}>
+        <div className="book-cover" style={bookCover} data-testid="book-cover">
+          <div className="book-cover-overlay" />
         </div>
 
-        <div className="book-actions" data-testid="book-actions">
-          <BookActionButton action={book.action.type} onClick={this.props.onAction} />
+        <div className="book-details">
+          <h1 className="book-title">{book.title}</h1>
+          <h2 className="book-author">{book.author}</h2>
         </div>
+      </div>
 
-        {isOnUsersWaitlist && <WaitlistIndicator addedDate={book.waitlist_added_date} />}
-      </Paper>
-    );
-  }
+      <div className="book-actions" data-testid="book-actions">
+        <BookActionButton action={book.action.type} onClick={onAction} />
+      </div>
+
+      {isOnUsersWaitlist && <WaitlistIndicator addedDate={book.waitlist_added_date} />}
+    </Paper>
+  );
 }
 
 Book.propTypes = {
