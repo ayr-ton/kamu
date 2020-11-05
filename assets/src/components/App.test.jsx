59e1ed763d159af42132f55d2f9e1571ea04f45a
Mario Cecchi <mcecchi@thoughtworks.com>
2020-11-05 14:53:30 -0300

    Remove enzyme from App tests and fix act warnings

--- assets/src/components/App.test.jsx
+++ assets/src/components/App.test.jsx
@@ -1,33 +1,21 @@
 import React from 'react';
-import { MuiThemeProvider } from '@material-ui/core';
-import { mount } from 'enzyme';
-import { render, screen, waitFor } from '@testing-library/react';
-import { MemoryRouter } from 'react-router';
+import { fireEvent, screen, waitFor } from '@testing-library/react';
 import App from './App';
 import { getLoggedUser } from '../services/ProfileService';
 import {
-  getBook,
-  getBooksByPage, getLibraries, getMyBooks, getWaitlistBooks,
+  getBook, getBooksByPage, getLibraries, getMyBooks, getWaitlistBooks,
 } from '../services/BookService';
 
 import { currentUser } from '../../test/userHelper';
-import { lightTheme, darkTheme } from '../styling/themes';
 import { trackEvent } from '../utils/analytics';
 import { someBook } from '../../test/booksHelper';
+import renderWithRouter from '../../test/renderWithRouter';
 
 jest.mock('../services/ProfileService');
 jest.mock('../services/BookService');
 jest.mock('../utils/analytics');
 
-const createComponent = (route) => mount(
-  <MemoryRouter initialEntries={[route]}>
-    <App />
-  </MemoryRouter>,
-);
-
 describe('App', () => {
-  let component;
-
   beforeEach(() => {
     getLoggedUser.mockResolvedValue(currentUser);
     getBooksByPage.mockResolvedValue({ results: [] });
@@ -35,41 +23,43 @@ describe('App', () => {
     getWaitlistBooks.mockResolvedValue({ results: [] });
     getLibraries.mockResolvedValue({ results: [{ slug: 'bh', id: 1 }] });
     getBook.mockResolvedValue(someBook());
-    component = createComponent('/');
   });
 
   it('has a header with a logo and navigation buttons', () => {
-    render(<MemoryRouter initialEntries={['/']}><App /></MemoryRouter>);
+    renderWithRouter(<App />);
 
     expect(screen.getByRole('img', { name: /kamu logo/i })).toBeInTheDocument();
     expect(screen.getByRole('button', { name: /home/i })).toBeInTheDocument();
   });
 
   it('shows current user\'s borrow count on my books button', async () => {
-    render(<MemoryRouter initialEntries={['/']}><App /></MemoryRouter>);
+    renderWithRouter(<App />);
 
     await waitFor(() => expect(screen.getByRole('button', { name: /my books/i })).toHaveTextContent(currentUser.borrowed_books_count.toString()));
   });
 
   describe('routing', () => {
     it('routes to library selector when path is root', () => {
-      expect(findByTestID(component, 'library-selector').exists()).toBeTruthy();
+      renderWithRouter(<App />, { initialEntries: ['/'] });
+      expect(screen.getByTestId('library-selector')).toBeInTheDocument();
     });
 
-    it('routes to my books component when path is /my-books', () => {
-      component = createComponent('/my-books');
-      expect(findByTestID(component, 'my-books-wrapper').exists()).toBeTruthy();
+    it('routes to my books component when path is /my-books', async () => {
+      renderWithRouter(<App />, { initialEntries: ['/my-books'] });
+      expect(screen.getByTestId('my-books-wrapper')).toBeInTheDocument();
+      expect(await screen.findByText(/borrowed with me/i)).toBeInTheDocument();
     });
 
-    it('routes to library component when path is /library/:slug', () => {
-      component = createComponent('/libraries/bh');
-      expect(findByTestID(component, 'library-wrapper').exists()).toBeTruthy();
+    it('routes to library component when path is /library/:slug', async () => {
+      renderWithRouter(<App />, { initialEntries: ['/libraries/bh'] });
+      expect(screen.getByTestId('library-wrapper')).toBeInTheDocument();
+      expect(await screen.findByTestId('book-list-container')).toBeInTheDocument();
     });
 
     it('routes to book detail and library component when path is /library/:slug/book/:id', async () => {
-      component = createComponent('/libraries/bh/book/123');
-      expect(findByTestID(component, 'book-detail-loader').exists()).toBeTruthy();
-      expect(findByTestID(component, 'library-wrapper').exists()).toBeTruthy();
+      renderWithRouter(<App />, { initialEntries: ['/libraries/bh/book/123'] });
+      expect(screen.getByTestId('library-wrapper')).toBeInTheDocument();
+      expect(await screen.findByTestId('book-detail-wrapper')).toBeInTheDocument();
     });
   });
 
@@ -80,25 +70,30 @@ describe('App', () => {
 
     it('renders with theme stored in localStorage', () => {
       localStorage.setItem('theme', 'dark');
-      component = createComponent('/');
-      expect(component.find(MuiThemeProvider).props().theme).toEqual(darkTheme);
+      renderWithRouter(<App />);
+      expect(document.getElementsByTagName('body')[0].className).toEqual('dark');
     });
 
     it('renders with light theme if no default theme is stored in localStorage', () => {
-      expect(component.find(MuiThemeProvider).props().theme).toEqual(lightTheme);
+      renderWithRouter(<App />);
       expect(document.getElementsByTagName('body')[0].className).toEqual('light');
     });
 
     it('changes to dark theme when change theme button is clicked', () => {
-      findByTestID(component, 'change-theme-button').simulate('click');
-      expect(component.find(MuiThemeProvider).props().theme).toEqual(darkTheme);
+      renderWithRouter(<App />);
+
+      fireEvent.click(screen.getByRole('button', { name: /change theme/i }));
+
       expect(document.getElementsByTagName('body')[0].className).toEqual('dark');
       expect(trackEvent).toHaveBeenCalledWith('Preferences', 'Toggle Theme', 'dark');
     });
 
     it('stores chosen theme as default in local storage', () => {
-      findByTestID(component, 'change-theme-button').simulate('click');
-      localStorage.getItem('theme', 'dark');
+      renderWithRouter(<App />);
+
+      fireEvent.click(screen.getByRole('button', { name: /change theme/i }));
+
+      expect(localStorage.getItem('theme')).toEqual('dark');
     });
   });
 });
--- assets/test/renderWithRouter.js
+++ assets/test/renderWithRouter.js
@@ -1,5 +1,20 @@
+/* eslint-disable react/display-name */
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/prop-types */
+import React from 'react';
 import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
 
-export const renderWithRouter = (component) => render(component, { wrapper: MemoryRouter });
+function RouterWrapper(options) {
+  return ({ children }) => (
+    <MemoryRouter {...options}>
+      {children}
+    </MemoryRouter>
+  );
+}
+
+export const renderWithRouter = (component, options = {}) => render(component, {
+  wrapper: RouterWrapper(options),
+});
+
 export default renderWithRouter;
