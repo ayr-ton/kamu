26bcb37733686ce92bb006b4382fc81c502f8645
Erika Silva <erikasons@gmail.com>
2020-11-05 19:16:32 -0300

    remove enzyme from tests

--- assets/src/components/libraries/SearchBar.test.jsx
+++ assets/src/components/libraries/SearchBar.test.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import Close from '@material-ui/icons/Close';
 import { fireEvent } from '@testing-library/dom';
 import { render } from '@testing-library/react';
 import SearchBar from './SearchBar';
@@ -8,32 +6,36 @@ import SearchBar from './SearchBar';
 describe('SearchBar', () => {
   let searchBarComponent;
   const searchTerm = 'search term text';
-  const onChange = jest.fn();
 
   it('should render with empty search term', () => {
-    searchBarComponent = render(<SearchBar onChange={onChange} query="" />);
+    searchBarComponent = render(<SearchBar onChange={() => {}} query="" />);
     const input = searchBarComponent.getByPlaceholderText('Search by book title or author');
     expect(input.value).toBe('');
   });
 
   it('should render with passed search term', () => {
-    searchBarComponent = render(<SearchBar onChange={onChange} query="test search" />);
-    searchBarComponent.getByDisplayValue('test search');
+    searchBarComponent = render(<SearchBar onChange={() => {}} query="test search" />);
+    expect(searchBarComponent.getByDisplayValue('test search')).toBeInTheDocument();
   });
 
   it('should clear search term when click in close icon', () => {
-    searchBarComponent = shallow(<SearchBar onChange={onChange} query="" />);
-    searchBarComponent.setState({ searchTerm });
+    searchBarComponent = render(<SearchBar onChange={() => {}} query="" />);
 
-    searchBarComponent.find(Close).simulate('click');
+    const inputField = searchBarComponent.getByPlaceholderText('Search by book title or author');
 
-    expect(searchBarComponent.state().searchTerm).toEqual('');
+    fireEvent.change(inputField, { target: { value: 'test' } });
+    expect(inputField.value).toBe('test');
+
+    fireEvent.click(searchBarComponent.getByRole('img', { name: 'Close' }));
+    expect(inputField.value).toBe('');
   });
 
   it('should call onchange function passing the trimmed search term when textfield changes', () => {
+    const onChange = jest.fn();
+
     searchBarComponent = render(<SearchBar onChange={onChange} query="" />);
 
-    const inputField = searchBarComponent.getByPlaceholderText(/search by book title or author/i);
+    const inputField = searchBarComponent.getByPlaceholderText('Search by book title or author');
 
     fireEvent.change(inputField, { target: { value: `${searchTerm}` } });
 
@@ -41,12 +43,14 @@ describe('SearchBar', () => {
   });
 
   it('should not call onchange function when trimmed text does not change', () => {
-    searchBarComponent = render(<SearchBar onChange={onChange} query="test " />);
+    const onChangeMock = jest.fn();
 
-    const inputField = searchBarComponent.getByPlaceholderText(/search by book title or author/i);
+    searchBarComponent = render(<SearchBar onChange={onChangeMock} query="test " />);
+
+    const inputField = searchBarComponent.getByPlaceholderText('Search by book title or author');
 
     fireEvent.change(inputField, { target: { value: 'test  ' } });
 
-    expect(onChange).not.toHaveBeenCalledWith('test');
+    expect(onChangeMock).not.toHaveBeenCalledWith('test');
   });
 });
